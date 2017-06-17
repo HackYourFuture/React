c92599206d1f587f64a113460d2e5249519e56e4
Mattijn Lahuis <mattijnlahuis@gmail.com>
2017-06-17 15:34:28 +0200

    changed new clock from prompt to input fields

--- clocks/src/App.js
+++ clocks/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react'
-import PersistedComponent from './PersistedComponent'
+// import PersistedComponent from './PersistedComponent'
 import Clock from './Clock'
 import './App.css'
 
-export default class App extends PersistedComponent {
+export default class App extends React.Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
       clocks: [],
       newClock: {
         city: "",
         timezone: ""
-      },
-      weird: "very"
+      }
     }
 
-    this.renderAddClock = this.renderAddClock.bind(this)
     this.handleCityInput = this.handleCityInput.bind(this)
-    this.renderClocks = this.renderClocks.bind(this)
+    this.handleTzInput = this.handleTzInput.bind(this)
   }
 
 
@@ -33,15 +31,15 @@ export default class App extends PersistedComponent {
   }
 
   renderClocks() {
-    // const {clocks} = this.state
-    //
-    // return (
-    //   <div className="app--clocks">
-    //     <Clock/>
-    //
-    //     {clocks.map((clock, index) => this.renderClock(clock, index))}
-    //   </div>
-    // )
+    const {clocks} = this.state
+
+    return (
+      <div className="app--clocks">
+        <Clock/>
+
+        {clocks.map((clock, index) => this.renderClock(clock, index))}
+      </div>
+    )
   }
 
   renderClock(clock, index) {
@@ -58,19 +56,32 @@ export default class App extends PersistedComponent {
   }
 
   handleCityInput(event) {
-    // this.setState(
-    //   {
-    //     newClock: {
-    //       city: event.target.value
-    //     }
-    //   })
+    this.setState(
+      {
+        newClock: {
+          city: event.target.value,
+          timezone: this.state.newClock.timezone
+        }
+      })
+  }
+
+  handleTzInput(event) {
+    this.setState(
+      {
+        newClock: {
+          city: this.state.newClock.city,
+          timezone: event.target.value
+        }
+      }
+    )
   }
 
   renderAddClock() {
     return (
       <div className="app--add-clock">
-        <form onSubmit={this.handleSubmit}>
-          <input type='text' onChange={this.handleCityInput} />
+        <form onSubmit={this.onAddClick}>
+          <input type='text' value={this.state.newClock.city} onChange={this.handleCityInput} /><br />
+          <input type='text' value={this.state.newClock.timezone} onChange={this.handleTzInput} /><br />
           <input type="submit" value="Add new clock" />
         </form>
       </div>
@@ -81,20 +92,26 @@ export default class App extends PersistedComponent {
     const clocks = [...this.state.clocks]
     clocks.splice(index, 1)
 
-    // this.setState({clocks})
+    this.setState({clocks})
   }
 
   // This is done to bind this function to <this>.
   // instead of `onAddClick() { ... }`
   // so that we don't have to type `this.onAddClick.bind(this)`
-  onAddClick = () => {
-    const city     = prompt("What is the city?")
-    const timeZone = prompt("What is the Time Zone?")
-
-    const clock = {city, timeZone}
+  onAddClick = (event) => {
+    event.preventDefault()
+    const {city, timezone} = this.state.newClock
+    const clock = {city, timezone}
     const clocks = [...this.state.clocks, clock]
 
-    // this.setState({clocks})
+    this.setState(
+      {
+        clocks,
+        newClock: {
+          city: "",
+          timezone: ""
+        }
+      })
   }
 
 }
