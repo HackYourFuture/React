80df72e2f22a572f0b77a03da73482ffad9be8f0
essamalsaloum <es.alsaloum@gmail.com>
2017-07-21 20:03:32 +0200

    reset

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,21 +1,107 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './list'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+  class App extends React.Component {
+    constructor (props){
+        super(props);
+        this.state = {
+          todos: [
+               {
+                description: "Read more about React",
+                done:        true
+               } ,
+               {
+              description: "Do the homework",
+               done:        false
+              },
+            ],
+          newToDo:{
+            description: "",
+            done:        false
+           }
+        }
+
+    }
+
+    handleSubmit(event){
+      event.preventDefault()
+      const {newToDo} = this.state  
+      const todos = [...this.state.todos, newToDo]
+      this.setState({todos})
+      document.getElementById("addItem").reset();
+      }
+
+    handleChange(event){
+      this.setState({
+        newToDo: {
+          description: event.target.value,
+          done: false
+        }
+      })
+    }
+
+    renderAddItem (){
+      return(
+        <div>
+          <form id="addItem" onSubmit={this.handleSubmit.bind(this)}>
+            <input type="submit" value="Add" />
+            <input type="text"  onChange={this.handleChange.bind(this)}/>
+          </form>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+      )
+    }
 
-export default App;
+    onDelete= (index) => {
+      const todosList = this.state.todos
+      todosList.splice(index, 1)
+      this.setState({todosList})
+    }
+
+
+
+    handleCheckBoxChange = (index) => {
+      const todosList = this.state.todos
+      todosList[index].done = !todosList[index].done;
+      this.setState({
+        todosList
+      });
+    }
+
+    save = (editToDo,index) => {
+      const todosList = this.state.todos
+      todosList[index] = editToDo
+      this.setState({
+        todosList
+        });
+    }
+
+    render() {
+      let todoList=this.state.todos
+      todoList= todoList.map(function(item,index){
+        return( <List  description={item.description}
+          done ={item.done}
+          key={index} index={index}
+          onDelete={this.onDelete}
+          handleCheckBoxChange={this.handleCheckBoxChange}
+          save={this.save}
+        />);
+      }.bind(this))
+      return (
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>To Do List</h2>
+          </div>
+          <div className= "App-Body">
+          {todoList}
+          {this.renderAddItem()}
+          </div>
+          </div>
+      );
+    }
+
+  }
+
+  export default App;
