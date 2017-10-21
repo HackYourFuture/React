bc1d6e7c86aab442c59fd40f460784d0760f512d
Ramicode <rami.armuni@zaytoon.org>
2017-10-21 19:08:57 +0200

    a simple todo App using React

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import TodosList from './todosList'
+import Todo from './todo'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div className='main'>
+        <h1>Todo List App</h1>
+        {TodosList.map((todo, index) => {
+          return (
+          <Todo
+          todoDescription={todo.todoDescription}
+          todoState={todo.todoState}
+          />
+          )
+        })}
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
