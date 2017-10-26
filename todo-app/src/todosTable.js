1ce99e237ba472d2500a01ac6359435182071c6b
Ramicode <rami.armuni@zaytoon.org>
2017-10-27 01:05:30 +0200

    added Checkbox  to toggle the 'done' prop

---
+++ todo-app/src/todosTable.js
@@ -0,0 +1,15 @@
+let todosTable = [
+    {
+        todoDescription: 'Get out of bed',
+        done: true
+    },
+    {
+        todoDescription: 'Brush teeth',
+        done: false
+    },
+    {
+        todoDescription: 'Eat breakfast',
+        done: false
+    }
+]
+export default todosTable
