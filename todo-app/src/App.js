0b3668fe08ced12fd301b8878caa7a8be1bb9683
Ramicode <rami.armuni@zaytoon.org>
2017-10-28 15:20:31 +0200

    added "No todos" functionality

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -7,15 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Todo List App</h1>
-        {todosTable.map((todo, index) => {
+        <h1>Todo List App</h1>        
+        {todosTable.length!==0 ? todosTable.map((todo, index) => {
           return (
             <Todo key={index}
               todoDescription={todo.todoDescription}
               done={todo.done}
             />
           )
-        })}
+        }) : ("No Todos")}
       </div>
     )
   }
