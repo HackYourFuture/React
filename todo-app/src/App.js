73cf6773a46d6f2ee25c91cc2f7bae62afada0b2
Ramicode <rami.armuni@zaytoon.org>
2017-11-04 01:10:01 +0100

    added "Add" "Remove" "Update" functionality to todo app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,148 @@
-import React, { Component } from 'react';
-import todosTable from './todosTable'
+import React, { Component } from 'react'
+import todos from './todosTable'
 import Todo from './todo'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      todos: [],
+      newTodo: {
+        id: '',
+        description: '',
+        stage: ''
+      },
+      updatedTodo: {
+        description: '',
+      },
+      visibility: false,
+      clicked: ''
+    }
+
+    this.changeTodoStage = this.changeTodoStage.bind(this)
+    this.captureUserInput = this.captureUserInput.bind(this)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.deleteTodo = this.deleteTodo.bind(this)
+    this.updateTodo = this.updateTodo.bind(this)
+    this.changeButtonsVisibility = this.changeButtonsVisibility.bind(this)
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ todos })
+    }, 1000)
+  }
+
+  changeTodoStage(todoStage, todoID) {
+    const updatedTodos = this.state.todos.map(todo => {
+      if (todoID === todo.id) {
+        return Object.assign({}, todo, { stage: todoStage })
+      }
+      return todo;
+    })
+    this.setState({
+      todos: updatedTodos
+    });
+  }
+
+  changeButtonsVisibility(show, clickedBtn) {
+    this.setState({
+      visibility: show,
+      clicked: clickedBtn
+    })
+  }
+
+  captureUserInput(event) {
+    const updatedNewTodo = Object.assign({}, this.state.newTodo, {
+      [event.target.name]: event.target.value
+    })
+    this.setState({
+      newTodo: updatedNewTodo
+    })
+
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault()
+    const updatedNewTodo = Object.assign({}, this.state.newTodo, {
+      id: this.state.todos.length + 1,
+      done: false
+    })
+    const updatedTodos = [...this.state.todos, updatedNewTodo]
+    this.setState({
+      todos: updatedTodos
+
+    })
+  }
+
+  deleteTodo(deletedTodoID) {
+    const newTodos = this.state.todos.filter(todo => {
+      return todo.id !== deletedTodoID
+    })
+    this.setState({
+      todos: newTodos,
+      newTodo: {
+        description: ''
+      }
+    })
+  }
+
+  updateTodo(updatedTodoID) {
+    const updatedTodos = this.state.todos.map(todo => {
+      if (updatedTodoID === todo.id) {
+        return Object.assign({}, todo, { description: this.state.newTodo.description })
+      }
+      return todo
+    })
+    this.setState({
+      todos: updatedTodos
+    })
+  }
+
+  renderTodos(todos) {
+    if (todos.length === 0) {
+      return <div> Loading... </div>
+    }
+    return todos.map(todo => {
+      return (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          clickedBtn={this.state.clicked}
+          visibility={this.state.visibility}
+          onShow={this.changeButtonsVisibility}
+          onChange={this.changeTodoStage}
+          onDelete={this.deleteTodo}
+          onUpdate={this.updateTodo}
+        />
+      )
+    })
+  }
+
   render() {
     return (
       <div className='App'>
-        <h1>Todo List App</h1>        
-        {todosTable.length!==0 ? todosTable.map((todo, index) => {
-          return (
-            <Todo key={index}
-              todoDescription={todo.todoDescription}
-              done={todo.done}
-            />
-          )
-        }) : ("No Todos")}
-      </div>
-    )
+        <div>
+          <h1 > Todo List App </h1>
+          <div>
+            <form className='form' onSubmit={this.onFormSubmit}>
+              <input className='textArea'
+                name='description'
+                type='text'
+                placeholder='input a Todo Description'
+                value={this.state.newTodo.description}
+                onChange={this.captureUserInput}
+              />
+              <button className='submit'>Add</button>
+            </form>
+          </div>
+          <div>
+            {this.renderTodos(this.state.todos)}
+          </div>
+        </div>
+      </div>)
   }
 }
 export default App
