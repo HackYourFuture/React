78531333d5d098293770d267d65455b84ee8470b
meyadd <eyad.aldarra@gmail.com>
2017-12-12 01:30:01 +0100

    homework

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import Task from './Task';
+
+const todo_arr = [
+  {
+	 text:"Get out of bed" ,
+	 date:"Wed Sep 13 2017"
+  },
+  {
+	  text:"Brush teeth",
+	  date:"Thu Sep 14 2017"
+  },
+  {
+	  text:"Eat breakfast",
+	  date:"Fri Sep 15 2017"
+  }
+];
 
 class App extends Component {
+	
   render() {
     return (
       <div>
-        Hello World!
+        <h1>Todo list</h1>
+		<ul className='task-list'>
+			{todo_arr.map(item => <Task task={item} />)}
+		</ul>
       </div>
     );
   }
