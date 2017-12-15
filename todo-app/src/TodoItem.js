574ecffce9f513905ff0a7922a1f2a6b2104241f
pesh12 <ekocibar@hotmail.com>
2017-12-15 07:56:35 +0100

    Todo App Week1-Version1

---
+++ todo-app/src/TodoItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Description from './Description';
+import Deadline from './Deadline';
+
+
+export default class TodoItem extends React.Component {
+
+  render() {
+    const {todoItem} = this.props;
+
+    return (
+      <li className="TodoItem">
+        
+          <div className="Item-info">
+            <Description description={todoItem.description}/>
+            <Deadline deadline={todoItem.deadline}/>
+          </div>
+    
+      </li>
+    )
+  }
+
+}
