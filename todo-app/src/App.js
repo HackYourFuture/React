25eeb202ebb99f11c0adc3ad8705a43ac1dcfb77
meyadd <30845998+meyadd@users.noreply.github.com>
2017-12-15 21:59:30 +0100

    add keys to map function

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,14 +3,17 @@ import Task from './Task';
 
 const todo_arr = [
   {
+	 id:1,
 	 text:"Get out of bed" ,
 	 date:"Wed Sep 13 2017"
   },
   {
+	  id:2,
 	  text:"Brush teeth",
 	  date:"Thu Sep 14 2017"
   },
   {
+	  id:3,
 	  text:"Eat breakfast",
 	  date:"Fri Sep 15 2017"
   }
@@ -23,7 +26,7 @@ class App extends Component {
       <div>
         <h1>Todo list</h1>
 		<ul className='task-list'>
-			{todo_arr.map(item => <Task task={item} />)}
+			{todo_arr.map(item => <Task key={item.id} task={item} />)}
 		</ul>
       </div>
     );
