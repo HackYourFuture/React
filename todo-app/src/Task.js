15f305816d7090a18ac2ba9a7ed6f6cc47e47d46
meyadd <30845998+meyadd@users.noreply.github.com>
2017-12-21 22:17:28 +0100

    Update Task.js

--- todo-app/src/Task.js
+++ todo-app/src/Task.js
@@ -4,12 +4,31 @@ import TaskDate from './TaskDate';
 import './task.css';
 
 export default class Task extends React.Component {
+	
+  constructor(props) {
+    super(props);
+
+    // Events and bind to `this`
+    this.markAsDone = this.markAsDone.bind(this);
+  }
+
+  markAsDone(event) {
+    const checked = event.target.checked;
+    this.props.onDoneChange(checked, this.props.task.id);
+  }
+  
   render() {
-    const { text ,date } = this.props.task;
+    const { description ,deadline , done } = this.props.task;
+	const is_done = done? "done" : "";
 
     return (
-      <li className='task-item'>
-        <TaskText text={text} />, <TaskDate date={date} />
+      <li className={"task-item " + is_done}>
+	    <input
+            type='checkbox'
+            checked={done}
+            onChange={this.markAsDone}
+         />
+         <TaskText text={description} />, <TaskDate date={deadline} />
       </li>
     );
   }
