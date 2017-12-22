7851c4d549b5abf94963597996cd3f33fec6ecef
meyadd <30845998+meyadd@users.noreply.github.com>
2017-12-22 21:38:15 +0100

    Update Task.js

--- todo-app/src/Task.js
+++ todo-app/src/Task.js
@@ -5,31 +5,31 @@ import './task.css';
 
 export default class Task extends React.Component {
 	
-  constructor(props) {
+    constructor(props) {
     super(props);
 
-    // Events and bind to `this`
-    this.markAsDone = this.markAsDone.bind(this);
-  }
+        // Events and bind to `this`
+        this.toggleDone = this.toggleDone.bind(this);
+    }
 
-  markAsDone(event) {
-    const checked = event.target.checked;
-    this.props.onDoneChange(checked, this.props.task.id);
-  }
+    toggleDone(event) {
+        const checked = event.target.checked;
+        this.props.onDoneChange(checked, this.props.task.id);
+    }
   
-  render() {
-    const { description ,deadline , done } = this.props.task;
-	const is_done = done? "done" : "";
+    render() {
+        const { description , deadline , done } = this.props.task;
+        const isDone = done? "done" : "";
 
-    return (
-      <li className={"task-item " + is_done}>
-	    <input
-            type='checkbox'
-            checked={done}
-            onChange={this.markAsDone}
-         />
-         <TaskText text={description} />, <TaskDate date={deadline} />
-      </li>
-    );
-  }
+        return (
+            <li className={"task-item " + isDone}>
+                <input
+                    type='checkbox'
+                    checked={done}
+                    onChange={this.toggleDone}
+                />
+                <TaskText text={description} />, <TaskDate date={deadline} />
+            </li>
+        );
+    }
 }
