070a04e68a687493f0c9738bca3bb0b454ad2da9
Gino <gino@mbp-van-gino.home>
2017-12-25 12:23:38 +0100

    homework week2

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 import Task from './Task' 
-
-
-const tasks = [
-     {
-          id: "1",
-          description: "Wake up",
-          deadline: "Wed Sep 13 2017"
-  },
-     {
-          id: "2",
-          description: "React HomeWorks",
-          deadline: "Today"
-  },
-     {
-          id: "3",
-          description: "Breakfast",
-          deadline: "Today before 12:00"
-  }
-];
-
-
+import todoItems from './todo.json';
 
 
 export default class App extends Component {
-	
+    state = {
+    todos: todoItems
+  }
+
+handleTaskDone = (todoID)=>{
+    this.setState({
+            todos: this.state.todos.map(todo =>{
+                if (todo.id === todoID){
+                    todo.done =! todo.done
+                }
+                return todo
+            })
+        })
+    }
   render() {
+      const{todos} = this.state
     return (
       <div>
         <h1>Todo App</h1>
 		<ul className='tasks'>
-			{tasks.map(task => <Task key={task.id} task={task}/>
-    )}
+         {todos.map((todoItem, i)=>(
+        <Task key = {todoItem.id} handleTaskDone=
+        {this.handleTaskDone} todoItem = {todoItem}/>
+    
+    ))}
 		</ul>
       </div>
     );
   }
+  
+  
+      
 }
--- todo-app/src/Task.js
+++ todo-app/src/Task.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import AppTask from './AppTask';
 import AppDate from './AppDate';
+import './index.css';
 
-export default class Task extends React.Component {
+export default class Task extends React.Component{
 
+  handleTaskDone = () =>  this.props.handleTaskDone(this.props.todoItem.id)
+  
   render() {
+  const{todoItem, handleTaskDone} = this.props;
        return (
-            <li className="task-list"> 
-            Task: <AppTask description={this.props.task.description}/>
-            <br/>
-            Deadline: <AppDate deadline={this.props.task.deadline}/>
-            </li>
+            <li className="task-list">
+             <span>{todoItem.done ? <input type = 'checkbox' onClick={()=>handleTaskDone(todoItem.id)}/>
+              :
+              <input type = 'checkbox' checked onClick={()=> handleTaskDone(todoItem.id)}/>   
+             }
+                 </span>
+            <span> {todoItem.done? <span className = 'task-done'>
+                     Task: <AppDate deadline={todoItem.description}/>
+                      <br/>
+                     Deadline: <AppDate deadline={todoItem.deadline}/></span>
+                      :
+                  <span className = 'task-not-done'>
+                    Task: <AppDate deadline={todoItem.description}/>
+                      <br/>
+                    Deadline: <AppDate deadline={todoItem.deadline}/></span>
+}
+             </span>
+           
+            </li>  
+            
        )
+          
+      
   }
+      
+      
+       
+   
 }
