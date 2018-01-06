160fb727568f0032b4376317e7a2c05a5e364e43
itemah26 <ite.mah.26@gmail.com>
2018-01-06 23:33:04 +0100

    week2

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,39 +1,48 @@
 import React, { Component } from 'react';
 import Task from './Task' 
-
-
-const tasks = [
-     {
-          id: "1",
-          text: "Wake up",
-          deadline: "unlimited"
-  },
-     {
-          id: "2",
-          text: "React HomeWorks",
-          deadline: "Today"
-  },
-     {
-          id: "3",
-          text: "Breakfast",
-          deadline: "Today before 12:00"
-  }
-];
-
-
-
+import tasks from './tasks.json'
 
 export default class App extends Component {
-	
-  render() {
-    return (
-      <div>
-        <h1>Let's do this:</h1>
-		<ul className='tasks'>
-			{tasks.map(task => <Task key={task.id} task={task}/>
-    )}
-		</ul>
-      </div>
-    );
-  }
+
+     constructor (props){
+          super(props);
+          
+          this.state = {
+            tasks: tasks,
+       };
+          
+          this.handelDoneTasks = this.handelDoneTasks.bind(this);
+       
+     }
+     
+     handelDoneTasks(isDone,taskId){
+          const updatedTasks = this.state.tasks.map(task => {
+               if(taskId === task.id) {
+                    return Object.assign({}, task, {done: isDone});
+               }
+               return task;
+          });
+          this.setState({
+               tasks: updatedTasks,
+          });    
+     }
+     
+     render() {
+          if(this.state.tasks.length == 0){
+               return (
+               <div>
+                    <h1>Let's do this:</h1>
+                    <div>No items</div>
+               </div>
+                    );
+          }
+          return (
+               <div>
+                 <h1>Let's do this:</h1>
+                 <ul className='tasks'>
+                    {this.state.tasks.map(item => <Task key={item.id} task={item} onDone={this.handelDoneTasks} />)}
+		       </ul>
+               </div>
+          );
+     }
 }
--- todo-app/src/Task.js
+++ todo-app/src/Task.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import TaskText from './TaskText';
 import TaskDeadLine from './TaskDeadLine';
+import './index.css'
 
 export default class Task extends React.Component {
+     constructor(props) {
+          super(props);
+          
+          this.toggleDone = this.toggleDone.bind(this);
+    }
+     
+    toggleDone(event) {
+        const checked = event.target.checked;
+        this.props.onDone(checked, this.props.task.id);
+    }
+     
+    render() {
+        const { description , deadline , done } = this.props.task;
+        const isDone = done? "done" : "";
 
-  render() {
-       const  { text , deadline }  = this.props.task;
-       return (
-            <li className="task-list"> 
-            <TaskText text={text}/>
-            Deadline: <TaskDeadLine deadline={deadline}/>
+        return (
+            <li className={"task-list " + isDone}>
+                <input
+                    type='checkbox'
+                    checked={done}
+                    onChange={this.toggleDone}
+                />
+                <TaskText text={description} /><TaskDeadLine deadline={deadline} />
             </li>
-       )
-  }
+        );
+    }
 }
