60aa869e2db0693c9ad1d18e39903309ea792bbd
yousefallaban <yousefallaban@gmail.com>
2018-02-26 20:54:03 +0100

    todo-app HM

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import TodoList from './component/todoList';
+import Header from "./component/header";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World!
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div>
+            <Header />
+            <TodoList />
+         </div>
+      );
+   }
 }
 
 export default App;
---
+++ todo-app/src/component/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    render() {
+        return (
+            <div className="theme">
+                <h1>To-Do</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
