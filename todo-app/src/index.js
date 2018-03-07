d7bb2b5c61e05eb56aa09bae9866cfc086445c84
chileluk <32581924+chileluk@users.noreply.github.com>
2018-03-07 11:50:00 -0500

    update week2 homework

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Main from './Main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
