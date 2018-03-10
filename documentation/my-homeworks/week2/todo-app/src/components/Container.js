8866a36d913635a380bc49daa9c3742bb1dba2a5
moesi88 <moesi88@icloud.com>
2018-03-10 23:42:13 +0100

    homework week2 done -__-

---
+++ documentation/my-homeworks/week2/todo-app/src/components/Container.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Checkbox from './Checkbox'
+import '../App.css';
+
+export default class Container extends Component {
+
+    render() {
+        const {
+            todoItems,
+            handelDoneStatus
+        } = this.props
+
+        return (<div>
+            <ul>
+                {todoItems.map((item, index) => {
+                    return <li key={item.id} className="app-list">
+                        <Checkbox id={item.id}
+                            description={item.description}
+                            deadline={item.deadline}
+                            done={item.done}
+                            index={index}
+                            handelDoneStatus={handelDoneStatus} />
+                    </li>
+                })}
+            </ul>
+        </div>
+        )
+    }
+}
