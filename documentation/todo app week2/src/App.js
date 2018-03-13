b9e7db5cbf7a23fff57834c77b196df8f63c05bc
Nagham <naghamfarwati87@gmail.com>
2018-03-13 11:45:34 +0100

    homework week2

---
+++ documentation/todo app week2/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import List from './components/List';
+
+export default class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <List />
+      </div>
+    );
+  }
+}
