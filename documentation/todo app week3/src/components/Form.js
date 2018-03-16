a7aa0632e69c6983b1af7225e8fdbce2a157878f
Nagham <naghamfarwati87@gmail.com>
2018-03-16 11:55:06 +0100

    homework week3

---
+++ documentation/todo app week3/src/components/Form.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+export default class Form extends Component {
+
+    render() {
+        return (
+            <div className='Form'>
+                <form onSubmit={(event) => this.props.handleSubmit(event)}>
+                    
+                    <label> Task :    
+                       <input type='text' placeholder='discription' value={this.props.discription}
+                            onChange={(event) => this.props.handleDiscriptionChange(event)} 
+                        discription={this.props.discription}
+                       />
+                    </label>
+                    
+                    <label> Date :
+                       <input type='date' placeholder='deadline' value={this.props.deadline}
+                            onChange={(event) => this.props.handleDeadlineChange(event)}
+                        deadline={this.props.deadline}
+                       />
+                    </label>
+
+                    <button>Add</button>
+                </form>
+            </div>
+        )
+    }
+
+
+}
