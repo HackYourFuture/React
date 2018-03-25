c4ea25a7a016a773d51c6192ec38ada9a42c5e52
malnajar <m.n.friendi@gmail.com>
2018-03-25 12:27:02 +0200

    last Update

--- todo-app/src/localStorage.js
+++ todo-app/src/localStorage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const loadFromLocalStorage = data => {
 
     return JSON.parse(localStorage.getItem('todoItems'))
@@ -9,16 +8,4 @@ export const saveToLocalStorage = data => {
 
     return localStorage.setItem('todoItems', JSON.stringify(data))
 
-=======
-export const loadFromLocalStorage = data => {
-
-    return JSON.parse(localStorage.getItem('todoItems'))
-
-}
-
-export const saveToLocalStorage = data => {
-
-    return localStorage.setItem('todoItems', JSON.stringify(data))
-
->>>>>>> 859297d475b9ef92c6eeab7d5cb4cba98c6cc573
 }
