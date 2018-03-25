895268b17692d0a30e8bd4787a43e91df7773a8a
malnajar <m.n.friendi@gmail.com>
2018-03-25 12:14:52 +0200

    last update

--- todo-app/src/Components/DisplayDetails.js
+++ todo-app/src/Components/DisplayDetails.js
@@ -1,48 +1,80 @@
 import React, { Component } from 'react';
-import DeleteButton from './DeleteButton'
-import EditTask from './EditTask'
-import '../App.css';
-export default class DisplayDetails extends Component {
-    
-    handelDisplay = (id,index, selected, description, deadline, handelUpdateTask, handelDeleteTask) => {
-        if (selected === true) {
-            return (
-                <div className='TodoFunctionality-list-items'>
-                    <div><h2>#{id}</h2></div>
-                    <div><h2>{description}  [Deadline at:{deadline}]</h2></div>
+import { observer, inject } from 'mobx-react'
+import moment from 'moment'
 
-                    <div>
-                        <EditTask
-                        id={id}
-                        handelUpdateTask={handelUpdateTask}
-                        />
-                        <DeleteButton
-                        id={id}
-                        selected={selected}
-                        handelDeleteTask={handelDeleteTask}
-                        />
-                    </div>
-                </div>
-            )
-        
-        }
+import '../App.css';
+
+@inject('data')
+@observer
+export default class DisplayDetails extends Component {
+    handleTime = (timeLeft) => {
+        let taskDeadline = moment(timeLeft).toObject()
+        return moment().to(moment([taskDeadline.years, taskDeadline.months, taskDeadline.date]))
     }
     render() {
         const {
-            id,
-            description,
-            deadline,
-            selected,
-            index,
-            handelDeleteTask,
-            handelUpdateTask,
-        } = this.props
+            todoList,
+            selectedToEdit,
+            editedTask,
+            deleteTask,
+            updateTask,
+            selectToEdit,
+            changeEditedTask,
+            setDone
+        } = this.props.data
 
-
+        // console.log(moment('2018-2-2').toObject())
         return (
             <div>
-                {this.handelDisplay(id, index, selected, description, deadline, handelUpdateTask, handelDeleteTask)}
-                
+                {todoList.map((task) => {
+                    if (task.selected === true) {
+                        return (
+                            <li key={task.id} className="TodoFunctionality-list">
+                                
+                                <div className='TodoFunctionality-list-items'>
+                                    
+                                    <div>
+                                        <h2>#{task.id}</h2>
+                                    </div>
+
+                                    <div>
+                                        <h2>{task.description}</h2>  <h4><br/>Deadline at:  {this.handleTime(task.deadline)}</h4>
+                                    </div>
+
+                                    <div>
+
+                                        <div>
+                                            {selectedToEdit === task.id ?
+                                                <div>
+                                                    <input type="text" onChange={e => changeEditedTask(e)} value={editedTask} />
+                                                    <button onClick={() => updateTask(task.id, editedTask)}>Save</button>
+                                                    <button onClick={() => selectToEdit(null)}>Cancel</button>
+                                                </div> 
+                                                :
+                                                <button className='TodoFunctionality-list-items-buttons' onClick={() => selectToEdit(task.id)}>Edit</button>
+                                            }
+                                        </div>
+
+                                        <div>
+                                            <button className='TodoFunctionality-list-items-buttons' onClick={() => deleteTask(task.id, task.selected)}>Delete</button>
+                                        </div>
+
+                                        <div>
+                                            {task.isDone === false ?
+                                                <button className='TodoFunctionality-list-items-buttons' onClick={() => setDone(task.id)}>Done</button>
+                                                :
+                                                <button className='TodoFunctionality-list-items-buttons' onClick={() => setDone(task.id)}>UnDone</button>
+                                            }
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </li>
+                        )
+                    }
+                })}
             </div>  
         )
     }
