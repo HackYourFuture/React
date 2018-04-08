85ec9c3d3f0e47406bd51ba1ad6da76882d55cfc
abdullahWeb <abdullahsmsmieh@gmail.com>
2018-04-08 12:04:44 +0200

    Added from Abdullah Week1

---
+++ documentation/week1/src/components/Header.js
@@ -0,0 +1,16 @@
+/**
+ * Created by nasir on 25/02/2018.
+ */
+import React, {Component} from 'react';
+
+export default class Header extends Component {
+
+  render() {
+    return (
+        <header className="App-header">
+          <h1> Todo List</h1>
+        </header>
+    );
+  }
+}
+
---
+++ documentation/week1/src/components/Item1.js
@@ -0,0 +1,24 @@
+/**
+ * Created by nasir on 25/02/2018.
+ */
+import React, {Component} from 'react';
+
+export default class item1 extends Component {
+ 
+  render() {
+
+    const item1 = (
+      <h1>Get out of bed, Wed Sep 13 2017</h1>
+    );
+    return (
+      <div>
+  
+      {item1}
+
+      </div > 
+
+);
+
+}
+}
+
