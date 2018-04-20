b88ae91981f26d7a7f9ff40c6a5b66be160ebe2c
unknown <fadikurdi00@gmail.com>
2018-04-20 14:03:18 +0200

    homework W1

---
+++ documentation/homework/W1-homework/todo-app/src/components/Photo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Photo extends Component{
+
+    render() {
+        return (
+            <img src='https://cdn1.iconfinder.com/data/icons/financial-colored-icons-vol-2/128/080-512.png' className="App-photo" alt='todoImg'></img>
+            
+        );
+    }
+} 
