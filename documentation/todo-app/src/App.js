68acf2ee87fabfec02874c03bf181b5b7c01762a
Zydan1 <hamzazydan5@gmail.com>
2018-04-24 23:15:18 +0200

    homework2 Hamza

--- documentation/todo-app/src/App.js
+++ documentation/todo-app/src/App.js
@@ -1,37 +1,76 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todo from './Todo';
+import Todos from "./Todos.json";
+
+let IsDone = []
+
 class App extends Component {
-  
-  render() {
-    let tasks = [
-      {
-        task: " Get out of bed",
-        date: " Wed Sep 13 2017",
-        deadline: "same day"
+  constructor() {
+    super()
+    this.state = {
+      tasks: Todos,
+      msg: "No items"
 
-      },
-      {
-        task: " Brush teeth",
-        date: "Thu Sep 14 2017",
-        deadline: "same day"
 
-      },
-      {
-        task: " Eat breakfast",
-        date: " Fri Sep 15 2017",
-        deadline: "same day"
+    }
+  }
 
+  handelCheck = (checked, id) => {
+    let newState = this.state.tasks;
+    newState.map((ele, index) => {
+      if (ele.id === id) {
+        if (ele.done === true) {
+          ele.done = false;
+          this.setState({
+          })
+        } else {
+          ele.done = true
+        }
+        this.setState({
+          tasks: newState
+        })
+        IsDone = []
       }
-    ]
-    return (
-     <div className = "root">
-       {tasks.map((ele,i)=>
-<Todo task = {ele.task} date = {ele.date} deadline = {ele.deadline} key= {i}/>
+    })
+    for (let key in newState) {
+      IsDone.push(newState[key].done)
+    }
+    console.log(IsDone)
+    this.check(IsDone)
+  }
+  check = (array) => {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] = false) {
+        return true;
+      } else {
+        return false;
+      }
 
-       )}
-    
-     </div>
+    }
+
+
+  }
+  render() {
+    let tasks = this.state.tasks
+    return (
+      <div className="root">
+        {this.check ? tasks.map((task, i) =>
+
+          <div className="todo">
+            <h3> {task.description}</h3>
+
+            <h4>Deadline: {task.deadline}</h4>
+            <label class="checkbox">{task.done ? "Done" : "Not Done"}
+              <input type="checkbox" defaultChecked={task.done ? true : false} onChange={(e) => { this.handelCheck(task.done, task.id) }} />
+              <span class="checkmark"></span>
+            </label>
+
+          </div>
+
+        ) : this.state.msg}
+
+      </div>
     );
   }
 }
--- documentation/todo-app/src/Todo.js
+++ documentation/todo-app/src/Todo.js
@@ -1,20 +1,22 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 
 
-export default class Todo extends Component{
-  
-render(){
- let {task,date,deadline} = this.props;   
- return(
-<div className = "todo">
-<h3> {task}</h3>
-<span> Date : <strong>{date}</strong></span>
-<h4>Deadline: {deadline}</h4>
+export default class Todo extends Component {
+    handel = (e) => {
+      console.log(e.target.value)  
+     }
+    render() {
+        let { task, date, deadline, done , mark} = this.props;
+        return (
+            <div className="todo">
+                <h3> {task}</h3>
 
-</div>
+                <h4>Deadline: {deadline}</h4>
+                <label>{done}</label><input Checked ={mark} onChange={(e) => this.handel(e)} type="checkbox" className="check" />
+            </div>
 
- )   
-}
+        )
+    }
 
 
 
