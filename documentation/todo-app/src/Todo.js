d1ec4f18052230d7151eacf2515d7ceb01693759
zydan1 <33907400+zydan1@users.noreply.github.com>
2018-05-07 18:17:32 +0200

    Update Todo.js

--- documentation/todo-app/src/Todo.js
+++ documentation/todo-app/src/Todo.js
@@ -7,12 +7,12 @@ import { inject, observer } from 'mobx-react';
 @observer
 class Todo extends Component {
   render() {
-    const { deadline, remove, handelCheck, onsubmit, handelChangeDescripton, handelChangeDate } = this.props.StoreTasks;
+    const { deadline,Description, remove, handelCheck, onsubmit, handelChangeDescripton, handelChangeDate } = this.props.StoreTasks;
     return (
       <div className="root">
         <form onSubmit={(e) => onsubmit(e)}>
-          <input value={this.props.StoreTasks.Description} onChange={handelChangeDescripton} name="Description" placeholder=" enter your task" type="text" className="text" />
-          <input value={this.props.StoreTasks.deadline} onChange={handelChangeDate} type="date" name="deadline" className="date" />
+          <input value={Description} onChange={handelChangeDescripton} name="Description" placeholder=" enter your task" type="text" className="text" />
+          <input value={deadline} onChange={handelChangeDate} type="date" name="deadline" className="date" />
           <button type="submit" >Add</button>
         </form>
         {this.props.StoreTasks.tasks.map((ele, index) =>
