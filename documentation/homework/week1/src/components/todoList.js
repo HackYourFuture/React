e68c7a6d97d9b01f284c425135f4a9bb8f33f7fc
arasmr <earas446@gmail.com>
2018-05-12 02:01:44 +0200

    newFiles added

--- documentation/homework/week1/src/components/todoList.js
+++ documentation/homework/week1/src/components/todoList.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import TodoListItem from './TodoListItem';
+import TodoListItem from './todoListItem';
 import TodoAdd from './todoAdd';
 
 class TodoList extends Component {
