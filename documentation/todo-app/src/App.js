17cb3fc0b8051ec57058890faaa99cd9cc1b2edc
jamsrahimi <rahimi@protonmail.com>
2018-05-14 11:15:12 +0200

    Completed HW2!

--- documentation/todo-app/src/App.js
+++ documentation/todo-app/src/App.js
@@ -3,53 +3,57 @@ import React, { Component } from 'react';
 import './App.css';
 import TopHeader from './components/TopHeader';
 import ToDoList from './components/ToDoList';
-import listTodo from './listTodo.json'
+import todoData from './listTodo.json'
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      allTodo: [],
+      items: todoData,
     }
   }
+
   handleToggleDone = (todoId) => {
-    const attemptTodoList = this.state.allTodo.map
-    ((todoAtom) => {
-      if (todoAtom.id === todoId) {
+    const attemptTodoList = this.state.items.map((element) => {
+      if (element.id === todoId) {
         return {
-          ...todoAtom,
-          done: !todoAtom.done
+          ...element,
+          done: !element.done
         }
       }
-      return todoAtom
+      return element;
     })
-    this.setState({ allTodo: attemptTodoList })
-  }
-  
-  render() {
 
+    this.setState({ items: attemptTodoList })
+  }
+
+  render() {
+    const title = 'Todo List of Homework2';
     return (
       <div className="root">
         <div className="App">
           <TopHeader />
+          <h1>{title}</h1>
           <section>
-            <ToDoList
-              listTodo={listTodo}
-              allTodo={this.state.allTodo}
-              handleToggleDone={this.handleToggleDone}
-              
-            />
+            {this.state.items.map((element, indexOfEach) => {
+               return  < ToDoList
+              key = { indexOfEach }
+              todo = { element.description }
+              deadline = { element.deadline }
+              todoId = { element.id }
+              todoStatus = { element.done }
+              handleToggleDone = { this.handleToggleDone }
+                />
+            })
+            }
           </section>
         </div>
       </div>
     );
   }
-  // ????
   componentDidMount() {
-    this.setState({
-      allTodo: ToDoList,
-    })
+    
   }
 }
 
--- documentation/todo-app/src/components/ToDoList.js
+++ documentation/todo-app/src/components/ToDoList.js
@@ -1,25 +1,18 @@
 
 import React, { Component } from 'react';
-import ToDoOutput from './ToDoOutput';
 
 export default class ToDoList extends Component {
 
     render() {
-        const title = 'Todo List of Homework2';
-        const { listTodo, allTodo, handleToggleDone } = this.props;
+        const { todoId, todo, deadline, handleToggleDone, todoStatus } = this.props;
+
         return (
             <div>
-                <h1>{title}</h1>
-                {listTodo.length === 0 ? <p>No items... </p> :
-                    listTodo.map((element, indexOfEach) => (
-                        <ToDoOutput
-                            todo={element.description}
-                            deadline={element.deadline}
-                            key={element.id}
-                            handleToggleDone={handleToggleDone}
-                        />
-                    ))
-                }
+                <div onClick={() => handleToggleDone(todoId)}  >
+                    {todoStatus ? '✔️  ' : '⬜  '}
+                    <span style={todoStatus ? { textDecoration: 'line-through' } : {}}><strong>{todo}</strong>{deadline}</span>
+                </div>
+
 
             </div>
         )
