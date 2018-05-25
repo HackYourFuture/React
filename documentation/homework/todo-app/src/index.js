d70749b7b6c389fefec9c468fe93d8ae0010adfc
Kemirdin Kaynam <qemirdin@gmail.com>
2018-05-25 22:46:31 +0200

    this is my homework about todo list

--- documentation/homework/todo-app/src/index.js
+++ documentation/homework/todo-app/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import {
+    render
+} from "react-dom";
+import Todo from "./Todo";
+import "./style.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => ( <div className = "container" >
+    <div className = "app" >
+    <div className = "header" >
+    <p > K3M1RD1N's Todo App</p> </div> <div className = "controls" >
+    <i className = "fa fa-plus active" aria-hidden = "true" / >
+    <i className = "fa fa-search"
+    aria-hidden = "true" / >
+    </div> <Todo / >
+    </div> </div>
+);
+
+render( < App / > , document.getElementById("root"));
