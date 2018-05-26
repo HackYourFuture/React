aaf57b674d7e9b2f4a601d197a8e81fcd47885ec
Sam <hackoor83@gmail.com>
2018-05-26 16:25:58 +0200

    Sam's homework first week React

--- documentation/todo-app/src/App.js
+++ documentation/todo-app/src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import AddTaskItem from "./components/AddTaskItem";
+import TasksList from "./components/TasksList";
 
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Sam's first homework Week 1</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <p> The static todo list: </p>
+          <AddTaskItem />
+          <TasksList />
+        </div>
       </div>
     );
   }
