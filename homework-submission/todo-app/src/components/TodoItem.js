f967c68fdf04d01ed0a90565b35d580c23b666bc
Gaorieh Isaac <gaorieh@hotmail.com>
2018-05-31 18:32:54 +0200

    update 7 - use uuid and state not in array

--- homework-submission/todo-app/src/components/TodoItem.js
+++ homework-submission/todo-app/src/components/TodoItem.js
@@ -2,28 +2,29 @@ import React, { Component } from 'react';
 import TodoList from './TodoList';
 const uuid = require('uuid/v4');
 
+
 class TodoItems extends Component {
 
     render() {
 
         const items = [
             {
-                'id': uuid(),
+
                 'task': 'Get out of bed',
                 'date': 'Wed Sep 13 2017'
             }, {
-                'id': uuid(),
+
                 'task': 'Brush teeth',
                 'date': 'Thu Sep 14 2017'
             }, {
-                'id': uuid(),
+
                 'task': 'Eat breakfast',
                 'date': 'Fri Sep 15 2017'
             }
         ];
 
         const TodoListItem = items.map((element) => (
-            <TodoList key={element.id} todoTask={element.task} taskDate={element.date
+            <TodoList key={uuid()} todoTask={element.task} taskDate={element.date
             } />
         ));
 
