3d10a790e8166578600a2ba1adf0f6c7d32fe090
Gaorieh Isaac <gaorieh@hotmail.com>
2018-05-31 18:17:32 +0200

    update 5 - use uuidv4

--- homework-submission/todo-app/src/components/TodoItem.js
+++ homework-submission/todo-app/src/components/TodoItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import TodoList from './TodoList';
+const uuid = require('uuid/v4');
 
 class TodoItems extends Component {
 
@@ -7,20 +8,25 @@ class TodoItems extends Component {
 
         const items = [
             {
-                task: 'Get out of bed',
-                date: 'Wed Sep 13 2017'
+                'id': uuid(),
+                'task': 'Get out of bed',
+                'date': 'Wed Sep 13 2017'
             }, {
-                task: 'Brush teeth',
-                date: 'Thu Sep 14 2017'
+                'id': uuid(),
+                'task': 'Brush teeth',
+                'date': 'Thu Sep 14 2017'
             }, {
-                task: 'Eat breakfast',
-                date: 'Fri Sep 15 2017'
+                'id': uuid(),
+                'task': 'Eat breakfast',
+                'date': 'Fri Sep 15 2017'
             }
         ];
+
         const TodoListItem = items.map((element, index) => (
-            <TodoList key={index} todoTask={element.task} taskDate={element.date
+            <TodoList key={element.id} todoTask={element.task} taskDate={element.date
             } />
         ));
+
 
         return (
             <div>
