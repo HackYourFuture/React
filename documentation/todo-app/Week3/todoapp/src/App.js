f46e25c3c0293ab897ef41a61337a56379f7d63c
Sam <hackoor83@gmail.com>
2018-06-01 15:20:35 +0200

    Week 3 updated

--- documentation/todo-app/Week3/todoapp/src/App.js
+++ documentation/todo-app/Week3/todoapp/src/App.js
@@ -1,10 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
-// import TodoJsonList from './TodoJsonList';
+import TodoJsonList from './TodoJsonList';
 import InputForm from './components/InputForm';
 import ItemsList from './components/ItemsList';
 
 class App extends Component {
+
+  state = {
+    TodoJsonList
+  }
+
+  addTodo = (event) => {
+    event.preventDefault();
+    const itemInput = this.itemInput.current.value;
+    const deadlineInput = this.deadlineInput.current.value;
+
+    if (!itemInput || !deadlineInput) {
+      alert('Please fill up');
+    }
+
+    this.setState((prevState) => {
+      let lastList = prevState.TodoJsonList;
+
+      lastList.push({
+        id: Date.now(),
+        description: itemInput,
+        deadline: deadlineInput,
+        done: false
+      });
+
+      return {
+        TodoJsonList: lastList
+      }
+
+    })
+
+    this.itemInput.current.value = '';
+    this.deadlineInput.current.value = '';
+  }
+
+  deleteTodo = (index) => {
+    this.setState((prevState) => {
+      let lastList = prevState.TodoJsonList;
+
+      lastList.splice(index, 1);
+      return {
+        TodoJsonList: lastList
+      }
+    })
+  }
+
+  checkItem = (todo, index, event) => {
+    this.setState((prevState) => {
+      let lastList = prevState.TodoJsonList;
+      todo.done = !todo.done;
+
+      lastList.splice(index, 1, todo);
+
+      return {
+        TodoJsonList: lastList
+      }
+
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,111 +74,14 @@ class App extends Component {
           Below is my TodoApp:
         </p>
 
-        <InputForm />
+        <InputForm addItem={this.addTodo} />
 
-        {/* <form className='input-form' onSubmit={this.addTodo}>
-          <input
-            className='todo-input'
-            type='text'
-            placeholder='Add the task here, then press ENTER'
-            // onKeyUp={this.addTodo}
-            ref={this.itemInput}
-          />
-          <input className='deadline-input'
-            type='date'
-            placeholder='Pick your deadline'
-            ref={this.deadlineInput}
-          />
-          <button type='submit' className='add-button'>Add </button>
-        </form> */}
-
-
-        <ItemsList />
-
-        {/* <div className='list-format'>
-          <div>
-            {this.state.TodoJsonList.map((todo, index) => {
-              return (
-                <div key={index + 1} className='list-format'>
-                  <p>{index + 1}</p>
-                  <input type='checkbox' className='checkBox' onChange={(event) => { this.checkItem(todo, index, event) }} />
-                  <p className={'todo-desc' + (todo.done ? ' completed' : '')}><strong>Description: </strong>{todo.description}.</p>
-                  <p className={(todo.done ? ' completed' : '')}><strong>Deadline: </strong>{todo.deadline}</p>
-                  <button className='remove-item' onClick={(event) => this.deleteTodo(index)}> X </button>
-                </div>
-              );
-            })}
-          </div>
-
-        </div> */}
+        <ItemsList deleteItem={this.deleteTodo} completeItem={this.checkItem} />
 
       </div>
     );
   }
 
-
-  // state = {
-  //   TodoJsonList
-  // }
-
-  // itemInput = React.createRef();
-  // deadlineInput = React.createRef();
-
-  // addTodo = (event) => {
-  //   event.preventDefault();
-  //   // if (event.key === 'Enter') {
-  //   const itemInput = this.itemInput.current.value;
-  //   const deadlineInput = this.deadlineInput.current.value;
-
-  //   if (!itemInput || !deadlineInput) {
-  //     alert('Please fill up');
-  //   }
-
-  //   this.setState((prevState) => {
-  //     let lastList = prevState.TodoJsonList;
-
-  //     lastList.push({
-  //       id: Date.now(),
-  //       description: itemInput,
-  //       deadline: deadlineInput,
-  //       done: false
-  //     });
-
-  //     return {
-  //       TodoJsonList: lastList
-  //     }
-
-  //   })
-
-  //   this.itemInput.current.value = '';
-  //   this.deadlineInput.current.value = '';
-  // }
-
-  // deleteTodo = (index) => {
-  //   this.setState((prevState) => {
-  //     let lastList = prevState.TodoJsonList;
-
-  //     lastList.splice(index, 1);
-  //     return {
-  //       TodoJsonList: lastList
-  //     }
-  //   })
-  // }
-
-  // checkItem = (todo, index, event) => {
-  //   this.setState((prevState) => {
-  //     let lastList = prevState.TodoJsonList;
-  //     todo.done = !todo.done;
-
-  //     lastList.splice(index, 1, todo);
-
-  //     return {
-  //       TodoJsonList: lastList
-  //     }
-
-  //   })
-  // }
 }
-// }
 
 export default App;
--- documentation/todo-app/Week3/todoapp/src/components/InputForm.js
+++ documentation/todo-app/Week3/todoapp/src/components/InputForm.js
@@ -1,49 +1,17 @@
 import React, { Component } from 'react';
 import '../App.css';
-import TodoJsonList from '../TodoJsonList';
+// import TodoJsonList from '../TodoJsonList';
 
 class InputForm extends Component {
 
-    state = {
-        TodoJsonList
-    }
 
     itemInput = React.createRef();
     deadlineInput = React.createRef();
 
-    addTodo = (event) => {
-        event.preventDefault();
-        // if (event.key === 'Enter') {
-        const itemInput = this.itemInput.current.value;
-        const deadlineInput = this.deadlineInput.current.value;
-
-        if (!itemInput || !deadlineInput) {
-            alert('Please fill up');
-        }
-
-        this.setState((prevState) => {
-            let lastList = prevState.TodoJsonList;
-
-            lastList.push({
-                id: Date.now(),
-                description: itemInput,
-                deadline: deadlineInput,
-                done: false
-            });
-
-            return {
-                TodoJsonList: lastList
-            }
-
-        })
-
-        this.itemInput.current.value = '';
-        this.deadlineInput.current.value = '';
-    }
 
     render() {
         return (
-            <form className='input-form' onSubmit={this.addTodo}>
+            <form className='input-form' onSubmit={this.props.addItem}>
                 <input
                     className='todo-input'
                     type='text'
