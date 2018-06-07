d5c734c5238ceeb6ec08f19e1940e5c9c1ee0e05
Rashad <rushkurdi@gmail.com>
2018-06-07 18:12:24 +0200

    week1

---
+++ homework-submission/todo-app/src/components/Photo.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Photo extends Component {
+  render() {
+    return (
+      <img
+        src="http://runmum.wpengine.netdna-cdn.com/wp-content/uploads/sites/16/2014/11/to-do-list1.jpg"
+        className="App-photo"
+        alt="todoListPic"
+      />
+    );
+  }
+}
+
+export default Photo;
---
+++ homework-submission/todo-app/src/components/TodoItems.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ListItem from "./listItems";
+
+class TodoItems extends Component {
+  render() {
+    const items = [
+      {
+        task: "Get out of bed",
+        date: "Wed Sep 13 2017"
+      },
+      {
+        task: "Brush teeth",
+        date: "Thu Sep 14 2017"
+      },
+      {
+        task: "Eat breakfast",
+        date: "Fri Sep 15 2017"
+      }
+    ];
+
+    return (
+      <div>
+        {items.map(element => (
+          <ListItem todoTask={element.task} taskDate={element.date} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default TodoItems;
