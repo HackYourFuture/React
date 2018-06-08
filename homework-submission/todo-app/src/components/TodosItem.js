c496c80c49966d8c43ee083e7b88b6f3c056b102
Gaorieh Isaac <gaorieh@hotmail.com>
2018-06-08 10:25:25 +0200

    week3+EditTodo

--- homework-submission/todo-app/src/components/TodosItem.js
+++ homework-submission/todo-app/src/components/TodosItem.js
@@ -4,19 +4,52 @@ import '../components/Image.css'
 import Image from '../components/Image';
 
 class TodosItem extends Component {
+    constructor() {
+        super();
+        this.state = {
+            editing: false
+        }
+    }
+    componentDidMount() {
+
+        this.setState({ changedText: this.props.todo.description });
+    }
+    handleEditing(event) {
+        this.setState({ editing: true, changedText: this.props.description })
+    }
+    handleEditingDone(event) {
+
+        if (event.keyCode === 13) {
+            this.setState({ editing: false })
+        }
+    }
+    handleEditingChange(event) {
+        let _changedText = event.target.value;
+        this.setState({ changedText: _changedText });
+    }
 
     render() {
 
-        let { id, description, deadline, done } = this.props.todo;
+        let { id, deadline, done } = this.props.todo;
         let { toggleDone, removeTodo } = this.props;
+        let viewStyle = {};
+        let editStyle = {};
+        if (this.state.editing) {
+            viewStyle.display = 'none';
+        } else {
+            editStyle.display = 'none';
+        }
+
         return (
             <div className="comment">
+
                 <input
                     className="checkbox"
                     onClick={() => toggleDone(id)}
                     defaultChecked={done}
                     type="checkbox" />
                 <Image className="App-image" />
+
                 <span
                     style={done
                         ? {
@@ -25,7 +58,19 @@ class TodosItem extends Component {
                         : {}}>
                     <h2 className="text">Task:
                     </h2>
-                    <b>{description}</b>
+
+                    <b>{this.state.changedText}</b>
+
+                    <textarea
+                        type="text"
+                        onKeyDown={this
+                            .handleEditingDone
+                            .bind(this)}
+                        onChange={this
+                            .handleEditingChange
+                            .bind(this)}
+                        style={editStyle}
+                        value={this.state.changedText} />
                 </span>
 
                 <span
@@ -43,7 +88,11 @@ class TodosItem extends Component {
                     </button>
                 </span>
                 <span>
-                    <button>
+                    <button
+                        style={viewStyle}
+                        onClick={this
+                            .handleEditing
+                            .bind(this)}>
                         Edit Todo
                     </button>
                 </span>
