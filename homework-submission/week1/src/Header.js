a1e110d285c520ac9107f5e973adefe37825eab9
hadeel alabrash <hadeelalabrash@hadeels-air.home>
2018-06-14 16:53:51 +0200

    week1

---
+++ homework-submission/week1/src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header" >
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2 className="App-title" > Todo App </h2>
+            </header >
+        );
+    }
+}
+
+export default Header;
