96a6f0862e49b1e11f825bcc6683cc4273f872f3
MURATKAYMAZ56 <muratkaymaz56@gmail.com>
2018-06-17 14:13:12 +0200

    pulll managed

---
+++ homework-submission/week2/src/Aplication.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Checkbox from "./Checkbox";
+
+const items = ["One", "Two", "Three"];
+
+class Application extends Component {
+  componentWillMount = () => {
+    this.selectedCheckboxes = new Set();
+  };
+
+  toggleCheckbox = label => {
+    if (this.selectedCheckboxes.has(label)) {
+      this.selectedCheckboxes.delete(label);
+    } else {
+      this.selectedCheckboxes.add(label);
+    }
+  };
+
+  handleFormSubmit = formSubmitEvent => {
+    formSubmitEvent.preventDefault();
+
+    for (const checkbox of this.selectedCheckboxes) {
+      console.log(checkbox, "is selected.");
+    }
+  };
+
+  createCheckbox = label => (
+    <Checkbox
+      label={label}
+      handleCheckboxChange={this.toggleCheckbox}
+      key={label}
+    />
+  );
+
+  createCheckboxes = () => items.map(this.createCheckbox);
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <form onSubmit={this.handleFormSubmit}>
+              {this.createCheckboxes()}
+
+              <button className="btn btn-default" type="submit">
+                Save
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Application;
--- homework-submission/week2/src/App.js
+++ homework-submission/week2/src/App.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import todos from './todos.json';
-import Checkbox from './Checkbox';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import todos from "./todos.json";
+import Checkbox from "./Checkbox";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-    state = {
-        todos
-    }
+  state = {
+    todos
+  };
 
-    onChange = (event) => {
-        const id = event.target.id;
-        const todos = this.state.todos;
+  onChange = event => {
+    const id = event.target.id;
+    const todos = this.state.todos;
 
-        todos.forEach(todo => {
-            if (String(todo.id) === String(id)) {
-                todo.done = !todo.done;
-            }
-        });
+    todos.forEach(todo => {
+      if (String(todo.id) === String(id)) {
+        todo.done = !todo.done;
+      }
+    });
 
-        this.setState({ todos });
-    }
+    this.setState({ todos });
+  };
 
-    render () {
-        const todoItems = this.state.todos.map(todo => (
-            <Checkbox key={todo.id} todo={todo} onChange={this.onChange} />
-        ));
+  render() {
+    const todoItems = this.state.todos.map(todo => (
+      <Checkbox key={todo.id} todo={todo} onChange={this.onChange} />
+    ));
 
-        return (
-            <div className="App" >
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <ul>
-                    {todoItems.length > 0 ? todoItems : (<p>No items</p>)}
-                </ul>
-            </div>
-        );
-    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <ul>{todoItems.length > 0 ? todoItems : <p>No items</p>}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
