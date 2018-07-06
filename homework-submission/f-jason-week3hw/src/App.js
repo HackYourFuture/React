14ffe9a501e26b94bb2a1046040d16ae75f17fe5
Jason <jason.h.w.tan@gmail.com>
2018-07-06 23:06:27 +0200

    homework attempt 1

--- homework-submission/f-jason-week3hw/src/App.js
+++ homework-submission/f-jason-week3hw/src/App.js
@@ -1,18 +1,86 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import todoList from "./data/todos.json";
+import Form from "./components/Form";
+import TodoItem from "./components/TodoItem";
 
 class App extends Component {
+  state = {
+    todoList
+  };
+
+  onCheckboxChange = index => {
+    console.log(index);
+    if (index >= 0) {
+      todoList[index].done = !todoList[index].done;
+    }
+
+    this.setState({
+      todoList
+    });
+  };
+
+  onSubmitForm = event => {
+    event.preventDefault();
+
+    let id = 0;
+    if (todoList.length > 0) {
+      id = todoList[todoList.length - 1].id + 1;
+    }
+
+    let deadline = event.target.deadline.value;
+    let description = event.target.description.value;
+
+    let match = deadline.match(/^(\d{1,2})-(\d{1,2})-(\d{4})$/);
+    if (match && description.length > 0) {
+      let newTodo = {
+        id: id,
+        description: description,
+        deadline: deadline,
+        done: false
+      };
+      todoList.push(newTodo);
+
+      this.setState({
+        todoList
+      });
+    } else {
+      alert(
+        "The deadline should be in this format DD-MM-YYYY \n And the Todo must be at least 1 character"
+      );
+    }
+  };
+  onRemoveItemButton = (index, event) => {
+    event.preventDefault();
+    todoList.splice(index, 1);
+
+    this.setState({
+      todoList
+    });
+  };
+
   render() {
+    let renderList;
+    if (todoList.length > 0) {
+      renderList = todoList.map((todo, index) => {
+        return (
+          <TodoItem
+            key={index}
+            removeButtonHandler={this.onRemoveItemButton}
+            checkBoxHandler={this.onCheckboxChange}
+            index={index}
+            todo={todo}
+          />
+        );
+      });
+    } else {
+      renderList = <p> No items... </p>;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>Todos List</h1>
+        <Form submitForm={this.onSubmitForm} />
+        {renderList}
       </div>
     );
   }
