f3784bd3f449754cdbfeaf421481e91d6929a679
Zaher <zaherqasqas@gmail.com>
2018-07-12 21:38:43 +0200

    Submit homework Week3

---
+++ homework-submission/React-Week3/src/todoapp/Picture.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Photo extends Component {
+    render() {
+        return (
+            <img
+                src="https://i2.wp.com/www.inkstruck.com/wp-content/uploads/2016/08/printable-daily-planner-page-1.jpg"
+                className="picture"
+                alt="picture of doing list"
+            />
+        );
+    }
+}
+
+export default Photo;
