c9db91ad577871ebcc68133f7710e2f8c3ce930e
Jason <jason.h.w.tan@gmail.com>
2018-07-12 14:11:58 +0200

    temporary commit

--- homework-submission/h-jason-week4hw/src/App.js
+++ homework-submission/h-jason-week4hw/src/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Form from "./components/Form";
+import TodoItem from "./components/TodoItem";
+import "./App.css";
+import { observer, inject } from "mobx-react";
 
+@inject("todoListStore")
+@observer
 class App extends Component {
   render() {
+    const { listItems } = this.props.todoListStore;
+    let renderList;
+    if (listItems.length > 0) {
+      renderList = listItems.map((todo, index) => {
+        return <TodoItem key={index} index={index} todo={todo} />;
+      });
+    } else {
+      renderList = <p> No items... </p>;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>Todos List</h1>
+        <Form />
+        {renderList}
       </div>
     );
   }
---
+++ homework-submission/h-jason-week4hw/src/components/Description.js
@@ -0,0 +1,62 @@
+//remove injecting your store
+
+import React from "react";
+import Deadline from "./Deadline";
+import Button from "./Button";
+import { observer, inject } from "mobx-react";
+
+@inject("todoListStore")
+@observer
+export class Description extends React.Component {
+    render() {
+        const {
+            index,
+            description,
+            deadline,
+            updateInputHandler,
+            cancelButtonHandler,
+            isEditing
+        } = this.props;
+
+        return (
+            <div style={{ display: "inline-block", textDecoration: "inherit" }}>
+                <form
+                    style={{
+                        display: isEditing ? "inline-block" : "none",
+                        fontSize: "12px"
+                    }}
+                    onSubmit={updateInputHandler}
+                >
+                    <input
+                        type="text"
+                        name="edit_description_input"
+                        defaultValue={description}
+                        id={index}
+                    />
+                    <input
+                        style={{ fontSize: "12px" }}
+                        type="submit"
+                        name="submit"
+                        value="update"
+                    />
+                    <Button
+                        text={"Cancel"}
+                        class_name={"button"}
+                        index={index}
+                        clickHandler={cancelButtonHandler}
+                    />
+                </form>
+                <span
+                    style={{
+                        display: !isEditing ? "inline-block" : "none",
+                        textDecoration: "inherit"
+                    }}
+                >
+                    {description} <Deadline deadline={deadline} />
+                </span>
+            </div>
+        );
+    }
+}
+
+export default Description;
