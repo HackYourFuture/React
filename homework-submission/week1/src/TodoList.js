6b95c3cff4b62d74b940f0b1fec44f41c36999a9
Meazer <meazer.m@gmail.com>
2018-08-10 00:20:21 +0200

    add key to todoItem

--- homework-submission/week1/src/TodoList.js
+++ homework-submission/week1/src/TodoList.js
@@ -5,7 +5,7 @@ import './TodoList.css';
 
 class TodoList extends React.Component {
   render() {
-    const List = this.props.listObj.map((item) => <TodoItem description={item.description} deadline={item.deadline} />);
+    const List = this.props.listObj.map((item, i) => <TodoItem key={i} description={item.description} deadline={item.deadline} />);
     return (
       <div id='todoList'>
         <h3>Todo List</h3>
