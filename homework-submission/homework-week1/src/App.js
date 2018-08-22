00b80618b11c4e0af57b89f67662c8ed07cd23a3
Dumie1 <dumiedumara@gmail.com>
2018-08-22 22:48:56 +0200

    done styling and including error message if there are not items

--- homework-submission/homework-week1/src/App.js
+++ homework-submission/homework-week1/src/App.js
@@ -8,31 +8,47 @@ class App extends Component {
   state = {
     todos
   }
+
+  styles = {
+    'border': '3px solid #e8effc',
+    'width': '500px',
+    'padding': '25px',
+    'listStyleType': 'none',
+    'background': '#6cc5fc',
+    'textAlign': 'left',
+    'borderRadius': '10px'
+  };
+
+  renderTodos() {
+    if (this.state.todos.length === 0) return <p>No items...</p>
+    
+    return <ul>
+      {
+        todos.map((todos) => {
+
+          return <li key={todos.id} style={this.styles}>
+            <input type='checkbox' />
+            {todos.description} , {todos.deadline} , {todos.done ? 'Done' : 'Undone'}
+          </li>
+        })
+      }
+    </ul>
+  }
+
   render() {
     console.log(this.state.todos);
     return (
       <div className='App'>
+        
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Dumie's Todo List</h1>
+          <h1 className='App-title'>Todo List</h1>
         </header>
+
         <p className='App-intro'></p> 
       
-        <ul>
-          {
-            todos.map((todos)=> {
-              // return <li>{todos.id} - {todos.description} - {todos.deadline} - {todos.done ? 'Done' : 'Undone'}</li>;
-              return <li>
-                <input type='checkbox'/>
-                {todos.description} , {todos.deadline} , {todos.done ? 'Done' : 'Undone'}
-              </li> 
-            })
-          }
-        </ul>
-        
-         <Description descriptionAndDeadline='Get out of bed , Wed Sep 13 2017' />
-        <Description descriptionAndDeadline='Brush teeth , Thu Sep 14 2017' />
-        <Description descriptionAndDeadline='Eat breakfast , Fri Sep 15 2017' /> 
+        {this.renderTodos()}
+
       </div>
     );
   }
