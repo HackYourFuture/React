b2a0dad88eee5d511a902c70150fe4d11a041e35
neveenatik <nevine.atike@gmail.com>
2018-08-25 17:34:02 +0200

    Added key to all iterators

--- homework-submission/week2/src/App.js
+++ homework-submission/week2/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import ListHeader from './components/ListHeader.jsx';
-import List from './components/List.jsx';
-import TodoItem from './components/TodoItem.jsx';
+// import List from './components/List.jsx';
+// import TodoItem from './components/TodoItem.jsx';
 import TodoItems from './sources/todoItems.json';
 
 class App extends Component {
@@ -16,26 +16,33 @@ class App extends Component {
     if (indexOfItem >= 0) {
       targetItem.done === true ? targetItem.done = false : targetItem.done = true;
     } 
+    localStorage.setItem('items', JSON.stringify(newState.items));
     this.setState({ items: newState.items });
+  }
+
+  componentDidMount() {
+    let stateCopy = localStorage.state
+    if (stateCopy !== undefined) {
+      this.setState({
+        state: JSON.parse(stateCopy)
+      });
+    }
   }
   
   render() {
-    console.log(this.state.items);
     return <React.Fragment>
         <header className="App-header">
           <img src="https://russellinvestments.com/-/media/images/au/tiles/todolist.png" className="App-logo" alt="logo" />
-          <ListHeader className="todo-list" Title="Todo List" />>
+          <ListHeader className="todo-list" Title="Todo List" />
         </header>
-        <ul>{this.state.items.map(item => (
-          <div>
-            <input 
-            type="checkbox" 
-            className={(item.done === true ? "checked" : "unchecked")}
-            id={item.id} 
-            defaultChecked={item.done} onInput={() => this.handleChecked(item.id)}/>
-            <li key={item.id}>{item.description + " deadLine: " + item.deadline}</li>
-          </div>
-          ))} 
+        <ul>
+          {this.state.items.map(item => 
+            <div key={"wrapper" + item.id}>
+              <input key={"checkbox" + item.id} type="checkbox" className={item.done === true ? "checked" : "unchecked"} id={item.id} defaultChecked={item.done} onInput={() => this.handleChecked(item.id)} />
+              <li key={item.id}>
+                {item.description + " deadLine: " + item.deadline}
+              </li>
+            </div>)}
         </ul>
       </React.Fragment>;
   }
