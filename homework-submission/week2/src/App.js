6490c7431740e368c2393dcb483728a964f76549
neveenatik <nevine.atike@gmail.com>
2018-08-27 00:08:35 +0200

    Changed state and setting it dynamically to assign its value from local storage when that is available

--- homework-submission/week2/src/App.js
+++ homework-submission/week2/src/App.js
@@ -6,43 +6,62 @@ import ListHeader from './components/ListHeader.jsx';
 import TodoItems from './sources/todoItems.json';
 
 class App extends Component {
+  state = {
+    items: localStorage.items !== undefined ? JSON.parse(localStorage.items) : TodoItems
+  };
 
-  state = { items : TodoItems};
-
-  handleChecked = (id) => {
+  handleChecked = id => {
     let newState = Object.assign({}, this.state);
     const indexOfItem = newState.items.findIndex(item => item.id === id);
     let targetItem = newState.items[indexOfItem];
     if (indexOfItem >= 0) {
-      targetItem.done === true ? targetItem.done = false : targetItem.done = true;
-    } 
-    localStorage.setItem('items', JSON.stringify(newState.items));
+      targetItem.done === true
+        ? (targetItem.done = false)
+        : (targetItem.done = true);
+    }
+    localStorage.setItem("items", JSON.stringify(newState.items));
     this.setState({ items: newState.items });
-  }
+  };
 
   componentDidMount() {
     let itemsCopy = localStorage.items;
+    console.log("itemsCopy: ", itemsCopy);
+    // this.setState = { items: localStorage.getItem("items") !== null ? localStorage.getItem("items") : TodoItems };
+
     if (itemsCopy !== undefined) {
-      this.setState({items: JSON.parse(itemsCopy)});
+      this.setState({ items: JSON.parse(itemsCopy) });
     }
   }
-  
+
   render() {
-    return <React.Fragment>
+    return (
+      <React.Fragment>
         <header className="App-header">
-          <img src="https://russellinvestments.com/-/media/images/au/tiles/todolist.png" className="App-logo" alt="logo" />
+          <img
+            src="https://russellinvestments.com/-/media/images/au/tiles/todolist.png"
+            className="App-logo"
+            alt="logo"
+          />
           <ListHeader className="todo-list" Title="Todo List" />
         </header>
         <ul>
-          {this.state.items.map(item => 
+          {this.state.items.map(item => (
             <div key={"wrapper" + item.id}>
-              <input key={"checkbox" + item.id} type="checkbox" className={item.done === true ? "checked" : "unchecked"} defaultChecked={item.done} onInput={() => this.handleChecked(item.id)} />
+              <input
+                key={"checkbox" + item.id}
+                type="checkbox"
+                className={item.done === true ? "checked" : "unchecked"}
+                defaultChecked={item.done}
+                onInput={() => this.handleChecked(item.id)}
+              />
               <li key={item.id}>
                 {item.description + " deadLine: " + item.deadline}
               </li>
-            </div>)}
+            </div>
+          ))}
         </ul>
-      </React.Fragment>;
+      </React.Fragment>
+    );
   }
 }
 
