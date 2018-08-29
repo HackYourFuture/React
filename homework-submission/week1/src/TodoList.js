6e60fcc404dfb9d3464c65635a7159749822845f
neveenatik <nevine.atike@gmail.com>
2018-08-29 20:43:32 +0200

    Corrected returned App and TodoList components for more readability and reliability

--- homework-submission/week1/src/TodoList.js
+++ homework-submission/week1/src/TodoList.js
@@ -3,7 +3,11 @@ import todoItems from './todoItems.js';
 
 class TodoList extends Component {
 
-  todoItem = todoItems.map((item, i) => <li key={i}>{ "Task: " + item.description + " deadLine: " + item.deadLine}</li>);
+  todoItem = todoItems.map((item, i) => (
+    <li key={i}>
+    { "Task: " + item.description + " deadLine: " + item.deadLine}
+    </li>
+    ));
 
   render() {
     return <ul>{this.todoItem}</ul>;
