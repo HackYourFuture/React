561cec8ea69322542737176d3a12714270835da4
amirno1 <amir.noshir@gmail.com>
2018-09-13 19:16:06 +0200

    some changes after review

--- homework-submission/reactweek1/src/App.js
+++ homework-submission/reactweek1/src/App.js
@@ -13,110 +13,139 @@ class App extends Component {
     this.state = {
       list: [
         {
-          "id": 1,
-          "description": "Get out of bed",
-          "deadline": "2017-09-11",
-          "done": true
+          id: 1,
+          description: "Get out of bed",
+          deadline: "2017-09-11",
+          done: true
         },
         {
-          "id": 2,
-          "description": "Brush teeth",
-          "deadline": "2017-09-10",
-          "done": false
+          id: 2,
+          description: "Brush teeth",
+          deadline: "2017-09-10",
+          done: false
         },
         {
-          "id": 3,
-          "description": "Eat breakfast",
-          "deadline": "2017-09-09",
-          "done": false
-        },
+          id: 3,
+          description: "Eat breakfast",
+          deadline: "2017-09-09",
+          done: false
+        }
       ],
-      newDescription: '',
-      newDeadline: ''
-    }
+      newDescription: "",
+      newDeadline: ""
+    };
   }
 
-  changeToDone = (event) => {
+  changeToDone = event => {
     const id = event.target.id;
-    const listCopy = this.state.list.slice(0);
-    const itemIndex = listCopy.findIndex(el => { return el.id === Number(id) });
+    const listCopy = [...this.state.list];
+    const itemIndex = listCopy.findIndex(el => {
+      return el.id === Number(id);
+    });
     listCopy[itemIndex].done = !listCopy[itemIndex].done;
     this.setState({
       list: listCopy
     });
-  }
-  descriptionHandler = (newDescription) => {
-    const state = this.state;
-    state['newDescription'] = newDescription;
-    this.setState(state)
+  };
+  descriptionHandler = newDescription => {
+    const listCopy = [...this.state];
+    listCopy["newDescription"] = newDescription;
+    this.setState(listCopy);
   };
 
-  deadLineHandler = (newDeadLine) => {
-    const state = this.state;
-    state['newDeadline'] = newDeadLine;
-    this.setState(state)
+  // descriptionHandler = newDescription => this.setState({ newDescription }); does not work properly
+
+  deadLineHandler = newDeadLine => {
+    const listCopy = [...this.state];
+    listCopy["newDeadline"] = newDeadLine;
+    this.setState(listCopy);
   };
+
+  // deadLineHandler = newDeadLine => this.setState({ newDeadLine }); does not work properly
 
   addHandler = () => {
     const state = this.state;
     const newItem = {
-      'id': this.state.list.length + 1,
-      'description': this.state.newDescription,
-      'deadline': this.state.newDeadline,
-      'done': false
+      id: this.state.list.length + 1,
+      description: this.state.newDescription,
+      deadline: this.state.newDeadline,
+      done: false
     };
-    this.state.list.push(newItem);
-    // this.state.newDeadline = '';
-    // this.state.newDeadline = ''; commented out these two cus it faces some problem through the test but dont know what the better way is
-    this.setState(state)
+    ([...this.state.newDeadline] = ""), ([...this.state.newDescription] = "");
+
+    this.setState({ list: [...state.list, newItem] });
   };
 
-  removeHandler = (i) => {
-    const state = this.state;
-    this.state.list.splice(i, 1);
-    this.setState(state);
+  removeHandler = i => {
+    let state = this.state;
+    const updatedList = state.list.filter(prop => prop.id !== i + 1);
+    this.setState({ list: updatedList });
   };
 
   itemHandler() {
     if (this.state.list.length === 0) return <p>No items... </p>;
 
-    return <div>
-      <ul>
-        {
-          this.state.list.map((el, i) => {
-            return <div key={i}>
-              <li className='todoList'>
-                <button onClick={() => this.removeHandler(i)}>Remove</button>
-                <input id={el.id} type="checkbox" onChange={this.changeToDone} defaultChecked={el.done} />
-                <span style={{ textDecoration: el.done ? "line-through" : null }}>
-                  {el.id} {el.description} - {el.deadline}
-                </span>
-              </li>
-            </div>;
-          })
-        }
-      </ul>
-      <label>
-        Enter description :
-              <input type="text" onChange={(e) => this.descriptionHandler(e.target.value)} value={this.state.list.newDescription} />
-      </label>
-      <label>
-        Enter deadline :
-              <input type="text" onChange={(e) => this.deadLineHandler(e.target.value)} value={this.state.list.newDeadline} />
-      </label>
-      <button onClick={() => this.addHandler()} >Add</button>
-    </div>
+    return (
+      <div>
+        <ul>
+          {this.state.list.map((el, i) => {
+            return (
+              <div key={i}>
+                <li className="todoList">
+                  <button onClick={() => this.removeHandler(i)}>Remove</button>
+                  <input
+                    id={el.id}
+                    type="checkbox"
+                    onChange={this.changeToDone}
+                    defaultChecked={el.done}
+                  />
+                  <span
+                    style={{ textDecoration: el.done ? "line-through" : null }}
+                  >
+                    {el.id} {el.description} - {el.deadline}
+                  </span>
+                </li>
+              </div>
+            );
+          })}
+        </ul>
+        <label>
+          Enter description :
+          <input
+            type="text"
+            onChange={e => this.descriptionHandler(e.target.value)}
+            value={this.state.list.newDescription}
+          />
+        </label>
+        <fieldset>
+          <legend>Choose your date</legend>
+          <div>
+            <input
+              type="date"
+              min="2018-01-01"
+              onChange={e => this.deadLineHandler(e.target.value)}
+            />
+          </div>
+        </fieldset>
+        <button onClick={() => this.addHandler()}>Add</button>
+      </div>
+    );
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="https://chosencalled.files.wordpress.com/2015/07/sharp-res-28.jpg?w=1024&h=1024" className="todo-logo" alt="TodoList-logo" />
+          <img
+            src="https://chosencalled.files.wordpress.com/2015/07/sharp-res-28.jpg?w=1024&h=1024"
+            className="todo-logo"
+            alt="TodoList-logo"
+          />
           <h1 className="App-title">To Do List</h1>
         </header>
         <br />
         {this.itemHandler()}
-      </div>);
+      </div>
+    );
   }
 }
 
