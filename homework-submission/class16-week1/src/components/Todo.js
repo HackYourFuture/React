03336f3f481478974e51cfd3bedb17f46f33de13
AllouhM <mahmoud.allouh0@gmail.com>
2018-10-19 13:24:15 +0200

    amending code

--- homework-submission/class16-week1/src/components/Todo.js
+++ homework-submission/class16-week1/src/components/Todo.js
@@ -5,12 +5,8 @@ class Todo extends React.Component {
     const { description, deadline } = this.props;
     return (
       <li className="todo_item">
-        {/* <h2> */}
         Description <span className="todo_desc">{description}</span>
-        {/* </h2> */}
-        {/* <h3> */}
         Deadline <span className="todo_deadline">{deadline}</span>
-        {/* </h3> */}
       </li>
     );
   }
