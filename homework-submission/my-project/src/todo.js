708b2cc94a9ff31585cf9e2ec0edec4a2bb0ddfc
marwanaboudaoud <marwanaboudoud@gmail.com>
2018-10-20 12:28:25 +0200

    week1 homework

--- homework-submission/my-project/src/todo.js
+++ homework-submission/my-project/src/todo.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
+const todos = [
+    { id: 1, description: 'Do my dutch homework', deadline: 'FRI 19 SEP' },
+    { id: 2, description: 'React homework', deadline: 'SAT 20 SEP' },
+    { id: 3, description: 'preaper to React week2 ', deadline: 'SUN 21 SEP' }
 
-class Todos extends React.Component {
-    state = {
-        todos: [
-            { id: 1, description: 'Do my dutch homework', deadline: 'FRI 19 SEP' },
-            { id: 2, description: 'React homework', deadline: 'SAT 20 SEP' },
-            { id: 3, description: 'preaper to React week2 ', deadline: 'SUN 21 SEP' }
+]
 
-        ]
-    }
+class TodosList extends React.Component {
+    item = todos.map((item, i) => (
+        <li key={i}>
+            {item.id + "- " + item.description + " DeadLine: " + item.deadline}
+        </li>
+    ));
     render() {
-        return (
-            <div class="item">
-                {this.state.todos.map(item => <li>{item.id}:{item.description}, Deadline:{item.deadline}</li>)}
-            </div>
-        )
+        return <ul>{this.item}</ul>;
     }
 }
-export default Todos 
+
+export default TodosList 
