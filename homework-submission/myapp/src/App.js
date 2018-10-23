26ab2ee423a84330edb4109af4cdf75ee42e4590
Dumie1 <dumiedumara@gmail.com>
2018-10-23 13:05:12 +0200

    added functionality to check if a todo is done (isTaskDone)

--- homework-submission/myapp/src/App.js
+++ homework-submission/myapp/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Data from './Components/Data';
+import Header from './Components/Header';
 
 class App extends Component {
   
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-          <p>
-            Todo<span className='designLogo'>App</span>
-          </p>
-          
-        </header>
-        <ul>
-            <Data description='Get out of bed' deadline='Wed Sep 13 2018' />
-            <Data description='Brush teeth' deadline='Thu Sep 14 2018'/>
-            <Data description='Eat breakfast' deadline='Fri Sep 15 2018' />
-            <hr />
-          </ul>     
+        
+        <Header/>
+        <Data />
+        <hr />
+               
       </div>
     );
   }
--- homework-submission/myapp/src/Components/Data.js
+++ homework-submission/myapp/src/Components/Data.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import Todos from './Todos';
 
 class Data extends React.Component {
 
+    state = {
+        Todos
+    }
+
+    isTaskDone = (todo) => {
+        todo.done = !todo.done;
+        this.setState({
+            todos: this.state.Todos
+        });
+    }
+
+    renderTodos() {
+        if (this.state.Todos.length === 0) return <p className='noItems'>No items...</p>
+
+        const textStyle = { textDecoration: 'line-through', backgroundColor: '#0455d8' }
+
+        return <ul>
+            {
+                Todos.map((todo, id) => {
+
+                    return (
+                        <li key={id}>
+                            <input checked={todo.done} type='checkbox' onChange={this.isTaskDone.bind(this, todo)} />
+                            <span style={todo.done ? textStyle : null}>
+                                {todo.description} , {todo.deadline}
+                            </span>
+                        </li>)
+                })
+            }
+        </ul>
+    }
+
     render() {
         return (
-                <li className='todos'>
-                     {this.props.description} , {this.props.deadline}
-                </li>
+            <div className='items'>
+                {this.renderTodos()}
+            </div>
         )
     }
 }
