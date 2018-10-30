5a14334745a5e634ef2eddc1a36f91fe59bf5a79
ward <vakpko@gmail.com>
2018-10-30 11:47:48 +0100

    fixed_checked_default

--- homework-submission/todo_react/src/App.js
+++ homework-submission/todo_react/src/App.js
@@ -1,48 +1,62 @@
-import React, { Component } from 'react';
-import './App.css';
-import Info from './Info';
-import data from './data';
-import TodoLists from './TodoLists';
-import ItemCreate from './ItemCreate'
-
+import React, { Component } from "react";
+import "./App.css";
+import Info from "./Info";
+import data from "./data";
+import TodoLists from "./TodoLists";
+import ItemCreate from "./ItemCreate";
 
 class App extends Component {
-    render() {
+  state = {
+    todos: data
+  };
 
-        const listData = data.map((item) => <TodoLists
-            key={item.id}
-            description={item.description}
-            deadline={item.deadline}
-            done={item.done}
+  handleClick = id => {
+    const newTodos = this.state.todos.slice();
 
-        />);
+    newTodos.forEach(todo => {
+      if (todo.id === id) {
+        todo.done = !todo.done;
+      }
+    });
+    this.setState({ todos: newTodos });
+  };
 
+  render() {
+    const listData = this.state.todos.map(item => (
+      <TodoLists
+        key={item.id}
+        description={item.description}
+        deadline={item.deadline}
+        done={item.done}
+        id={item.id}
+        handleClick={this.handleClick}
+      />
+    ));
 
-        if (listData.length === 0) {
-            return <div className="App"> <h3>Ther are no To-do's today!</h3></div>
-        }
-
-
-        return (
-
-            <div className="App">
-                {/* <DateP /> */}
-                <div>
-                    <ItemCreate />
-
-                </div>
-
-                {listData}
-
-
-
-                <div> <Info /> </div>
-                {/* {console.log(new Date())} */}
-
-
-            </div>
-        );
+    if (listData.length === 0) {
+      return (
+        <div className="App">
+          <h3>Ther are no To-do's today!</h3>
+        </div>
+      );
     }
+
+    return (
+      <div className="App">
+        {/* <DateP /> */}
+        <div>
+          <ItemCreate />
+        </div>
+
+        {listData}
+
+        <div>
+          <Info />
+        </div>
+        {/* {console.log(new Date())} */}
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- homework-submission/todo_react/src/TodoLists.js
+++ homework-submission/todo_react/src/TodoLists.js
@@ -1,72 +1,26 @@
-import React from 'react';
-import data from './data';
-import './TodoLists.css'
-
-
-
-const checkIfDone = data.map((item) => {
-    return item.done;
-})
-
-// let xerov = () => {
-
-
-//     for (let i = 0; i < checkIfDone.length; i++) {
-//         if (checkIfDone[i] === true) {
-//             alert('huaawei')
-//         } else {
-//             alert('klir');
-//         }
-//     }
-// }
-
-console.log(checkIfDone);
+import React from "react";
+import "./TodoLists.css";
 
 export default class TodoLists extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-
-            isChecked: '',
-        }
-    }
-
-    handleChecked = () => {
-        for (let i = 0; i < checkIfDone.length; i++) {
-            if (checkIfDone[i] === true) {
-                this.setState({ isChecked: true })
-            } else {
-                this.setState({ isChecked: false })
-            }
-        }
-
-
-    }
-
-    render() {
-        let { description, deadline, done } = this.props;
-        // let xxx = new Date(1 / 11 / 1999)
-
-
-        return (
-
-            <div className='list'>
-
-                <ul>
-
-                    <li>
-                        <input type="checkbox" className="myCheck"
-                            onChange={this.handleChecked} /><label > {description} /{deadline}/ {done} </label>
-
-
-                    </li>
-                </ul>
-            </div>
-        );
-
-
-    }
-
-
+  render() {
+    let { description, deadline, done, id, handleClick } = this.props;
+    // let xxx = new Date(1 / 11 / 1999)
+    // console.log(id);
+    return (
+      <div className="list">
+        <ul>
+          <li>
+            <input
+              type="checkbox"
+              defaultChecked={done}
+              onChange={handleClick}
+            />
+            <label>
+              {description} /{deadline}/ {done}
+            </label>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
