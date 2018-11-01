d1e892bef30fd0648e18c4a81187be62d9dda881
Dumie1 <dumiedumara@gmail.com>
2018-11-01 10:44:11 +0100

    still yet to apply concept of deep copy

--- homework-submission/myapp/src/Components/Data.js
+++ homework-submission/myapp/src/Components/Data.js
@@ -2,37 +2,46 @@ import React from 'react';
 import Todos from './Todos.json';
 
 class Data extends React.Component {
+  state = {
+    Todos
+  };
 
-    state = {
-        Todos
-    }
 
+  isTaskDone = todo => {
+    todo.done = !todo.done;
+    this.setState({
+      Todos
+    });
+  };
 
-    isTaskDone = (todo) => {
-        todo.done = !todo.done;
-        this.setState({
-            Todos
-        });
-    }
+  render() {
+    if (this.state.Todos.length === 0)
+      return <p className="noItems">No items...</p>;
 
-    render() {
-        
-        if (this.state.Todos.length === 0) return <p className='noItems'>No items...</p>
+    const textStyle = {
+      textDecoration: "line-through",
+      backgroundColor: "#0455d8"
+    };
 
-        const textStyle = { textDecoration: 'line-through', backgroundColor: '#0455d8' }
-
-        return <ul className='items'>
-            {Todos.map((todo, id) => {
-              return <li key={id}>
-                  <input checked={todo.done} type='checkbox' onChange={() => this.isTaskDone(todo)} />
-                  <span style={todo.done ? textStyle : null}>
-                    {todo.description} , {todo.deadline}
-                  </span>
-                </li>;
-            })}
-          </ul>;
-    }
-
+    return (
+      <ul className="items">
+        {Todos.map((todo, id) => {
+          return (
+            <li key={id}>
+              <input
+                checked={todo.done}
+                type="checkbox"
+                onChange={(e) => this.isTaskDone(todo)}
+              />
+              <span style={todo.done ? textStyle : null}>
+                {todo.description} , {todo.deadline}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
 export default Data;
