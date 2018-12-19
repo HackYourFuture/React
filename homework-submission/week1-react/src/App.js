3443331f4a6cc05dc15c676a6515ea93a661ffdc
mehmetscoskun <mehmetsaidcoskun@gmail.com>
2018-12-19 23:16:09 +0100

    after comment1

--- homework-submission/week1-react/src/App.js
+++ homework-submission/week1-react/src/App.js
@@ -18,22 +18,18 @@ class App extends Component {
   render() {
     const {myTodoApp} = this.props;
 
-    const isEmpty = myTodoApp.data.todos.length < 1 || myTodoApp.data.todos === undefined;
+    const isEmpty = !myTodoApp.data.todos.length || !myTodoApp.data.todos;
 
-    const todoList = myTodoApp.data.todos.map((item, key)=> 
+    const todoList = myTodoApp.data.todos.map((item)=> 
     (item._id === myTodoApp.idToUpdate) ?
     <Update
     id={item._id}
     text={item.description}
     date={item.deadline}
-    index={key}
-    key={key}
     updateTodo={myTodoApp.updateTodo}
     />
     :
     <Todo
-    key={key}
-    index={key}
     changeDone = {myTodoApp.changeDone}
     deleteTodo = {myTodoApp.deleteTodo}
     editButton={myTodoApp.editButton}
