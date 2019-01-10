a13bc695160f789cbf2509098345f9baa8008fa1
RimSebai <40184200+RimSebai@users.noreply.github.com>
2019-01-10 19:11:15 +0100

    Update App.js

--- homework-submission/my-project/src/App.js
+++ homework-submission/my-project/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoItem from "./TodoItem"
-import Data from './Data'
+import Post from './Post';
+import Data from './Data';
 
 class App extends Component {
   render() {
-    const newData = Data.map(item => <TodoItem key={key.id} description={item.description} deadlineDate={item.deadlineDate} />)
+    const newData = Data.map(item => <Post key={item.id} description={item.description} deadlineDate={item.deadlineDate} />)
     return (
       <div className="App">
         {newData}
