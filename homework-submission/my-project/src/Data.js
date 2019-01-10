1f4e6d530e1fdd2aac2e71038ff513e02af837ca
RimSebai <40184200+RimSebai@users.noreply.github.com>
2019-01-10 18:45:32 +0100

    Update Data.js

--- homework-submission/my-project/src/Data.js
+++ homework-submission/my-project/src/Data.js
@@ -1,14 +1,16 @@
-
 const Data = [
   {
+    id: 1,
     description: "Get out of bed,",
     deadlineDate: "Wed Sep 13 2017"
   },
   {
+    id: 2,
     description: "Brush teeth,",
     deadlineDate: "Thu Sep 14 2017"
   },
   {
+    id: 3,
     description: "Eat breakfast,",
     deadlineDate: "Fri Sep 15 2017"
   }
