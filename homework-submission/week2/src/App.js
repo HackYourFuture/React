397b0b898ec0679caef7d14e0c42be6c30e65c12
Damla78 <rabiabostanci@yahoo.com>
2019-01-19 21:23:21 +0100

    Corrections

--- homework-submission/week2/src/App.js
+++ homework-submission/week2/src/App.js
@@ -5,38 +5,54 @@ import Description from './ToDo/Description';
 import DeadLines from './ToDo/DeadLine';
 import DoneCheckBox from './ToDo/DoneCheckBox';
 
-let isChecked = false;
-
-const todos = [
-  {
-    "id": 1,
-    "description": "Get out of bed",
-    "deadline": "2017-09-11",
-    "done": true
-  },
-  {
-    "id": 2,
-    "description": "Brush teeth",
-    "deadline": "2017-09-10",
-    "done": false
-  },
-  {
-    "id": 3,
-    "description": "Eat breakfast",
-    "deadline": "2017-09-09",
-    "done": false
-  }
-]
+let todos;
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          "id": 1,
+          "description": "Get out of bed",
+          "deadline": "2017-09-11",
+          "done": true
+        },
+        {
+          "id": 2,
+          "description": "Brush teeth",
+          "deadline": "2017-09-10",
+          "done": false
+        },
+        {
+          "id": 3,
+          "description": "Eat breakfast",
+          "deadline": "2017-09-09",
+          "done": false
+        }
+      ]
+    };
+  }
+
+  handleCheck = (x) => {
+    //console.log('Ik ben hier...');
+    let todos = this.state.todos;
+    todos[x].done = !todos[x].done;
+    this.setState({
+      state: todos
+    })
+  }
+
   render() {
+    const todos = this.state.todos;
     return (
       <div>
         <h2>ToDo List</h2>
         <ul>
-          {todos.map((eleTodo) => (
+          {todos.map((eleTodo, key) => (
             <li key={eleTodo.id}>
-              <DoneCheckBox done={eleTodo.done}></DoneCheckBox>
+              <DoneCheckBox index={key} done={eleTodo.done} handleCheck={this.handleCheck}></DoneCheckBox>
               <Description
                 todo={eleTodo.description}
                 deadline={<DeadLines deadline={new Date(eleTodo.deadline)}></DeadLines>}
--- homework-submission/week2/src/ToDo/DoneCheckBox.js
+++ homework-submission/week2/src/ToDo/DoneCheckBox.js
@@ -1,28 +1,13 @@
 import React from 'react';
 
-let isDone = false;
-
 export default class DoneCheckBox extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { isDone: this.props.done };
-    this.handleCheck = this.handleCheck.bind(this);
-  }
-
-  handleCheck = () => {
-    this.setState({
-      isDone: !this.state.isDone
-
-    })
-  }
-
   render() {
 
     return (
       <input type="checkbox"
-        checked={this.state.isDone}
-        onChange={this.handleCheck} />
+        checked={this.props.done}
+        onChange={() => this.props.handleCheck(this.props.index)}
+      />
     );
   }
 }
