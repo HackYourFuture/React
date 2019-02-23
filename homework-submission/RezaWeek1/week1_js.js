e3eb7ff8df6a1ccc715728d01e3eab640ca6f50d
reza <reza_ranjbar94@yahoo.com>
2019-02-23 15:43:16 +0100

    second commit- not hardy code - no inline CSS

--- homework-submission/RezaWeek1/week1_js.js
+++ homework-submission/RezaWeek1/week1_js.js
@@ -2,12 +2,8 @@
 
 try {
   const ToDoItem = ({ description, deadline, done }) => {
-    const color = done ? 'red' : 'none';
-    // const decoration = {
-    //   'text-decoration': 'line-through',
-    // };
     return (
-      <li style={{ color }} className="item">
+      <li className={done ? 'done' : 'none'}>
         {description}, {deadline}, {done}
       </li>
     );
@@ -33,34 +29,14 @@ try {
     })
     .then(function(jsonResponse) {
       console.log(jsonResponse);
-      const ToDoListDynamic = () => (
-        <div className="list">
-          <p>This is a dynamic todo list:</p>
+
+      const ToDoListDynamic = () =>
+        jsonResponse.map(item => (
           <ul>
-            <ToDoItem
-              description={jsonResponse[0].description}
-              deadline={jsonResponse[0].deadline}
-              done={jsonResponse[0].done}
-            />
-            <ToDoItem
-              description={jsonResponse[1].description}
-              deadline={jsonResponse[1].deadline}
-              done={jsonResponse[1].done}
-            />
-            <ToDoItem
-              description={jsonResponse[2].description}
-              deadline={jsonResponse[2].deadline}
-              done={jsonResponse[2].done}
-            />
+            <ToDoItem description={item.description} deadline={item.deadline} done={item.done} />
           </ul>
-        </div>
-      );
-      // {jsonResponse.forEach(function(element) {
-      //   <ToDoItem description={element.description} deadline={element.deadline} />;
-      //   <li> hi </li>;
-      //   console.log('hi');
-      //   // element[0].description, ',', element[0].deadline;
-      // })}
+        ));
+
       class ToDoSDynamic extends React.Component {
         render() {
           return <ToDoListDynamic />;
@@ -75,7 +51,7 @@ try {
       );
     });
 
-  // The goal of this component is to create a dynamic to-do list with items from previous component .
+  // The goal of the component in this part is to create a dynamic to-do list with items from the JSON file .
 
   class ToDoStatic extends React.Component {
     render() {
