5ac3e36b9c27be177dbbcdb3062e1428af9dc442
Wael-Alhomsi <waelhom2@gmail.com>
2019-03-01 03:42:03 +0100

    corrected updateTodoList function

--- homework-submission/homework-week2/src/App.js
+++ homework-submission/homework-week2/src/App.js
@@ -8,21 +8,23 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { todo: arr };
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.updateTodoList = this.updateTodoList.bind(this);
   }
   deleteTodo(index) {
     this.setState({ todo: this.state.todo.filter(elem => elem !== this.state.todo[index]) });
   }
   updateTodoList(newTodo) {
-    const copiedArr = [...arr];
-    copiedArr.push(newTodo);
-    this.setState({ todo: copiedArr });
+    const newTodoArray = this.state.todo.slice();
+    newTodoArray.push(newTodo);
+    this.setState({ todo: newTodoArray });
   }
 
   render() {
     return (
       <div className="App">
         <TodoForm updateTodoList={data => this.updateTodoList(data)} />
-        <DynamicList todo={this.state.todo} deleteTodo={this.deleteTodo.bind(this)} />
+        <DynamicList todo={this.state.todo} deleteTodo={index => this.deleteTodo(index)} />
       </div>
     );
   }
