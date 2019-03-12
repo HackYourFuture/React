fdb35db36c9df1ad6d68c87a5698ce802329592b
Wael-Alhomsi <waelhom2@gmail.com>
2019-03-12 22:32:47 +0100

    added some modifications

--- homework-submission/homework-week3/react-hyf/react-hyf-client/src/HomeworkWeek3.js
+++ homework-submission/homework-week3/react-hyf/react-hyf-client/src/HomeworkWeek3.js
@@ -4,11 +4,10 @@ import OtherInformation from './components/OtherInformation';
 
 export default class HomeworkWeek3 extends Component {
   render() {
-    const data = this.props.data;
-    return data.map((elem, i) => (
-      <div className="individual" key={data[i].name}>
-        <FullName name={data[i].name} surname={data[i].surname} />
-        <OtherInformation gender={data[i].gender} region={data[i].region} />
+    return this.props.data.map(elem => (
+      <div className="individual" key={elem.name}>
+        <FullName name={elem.name} surname={elem.surname} />
+        <OtherInformation gender={elem.gender} region={elem.region} />
       </div>
     ));
   }
