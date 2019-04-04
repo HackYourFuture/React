c648e212b36a84a948b0cb7181dd4619fe9b0c2e
ammarvehbi <merakli.vehbi@gmail.com>
2019-04-04 23:55:57 +0200

    typo

--- homework-submission/week1/main.js
+++ homework-submission/week1/main.js
@@ -53,8 +53,8 @@ class App extends React.Component {
       <div>
         <Header title="Static List" />
         <ListItem description="Get out of bed" deadline="Wed Sep 13 2017" />
-        <ListItem description="Get out of bed" deadline="Wed Sep 13 2017" />
-        <ListItem description="Get out of bed" deadline="Wed Sep 13 2017" />
+        <ListItem description="Brush teeth" deadline="Thu Sep 14 2017" />
+        <ListItem description="Eat breakfast" deadline="Fri Sep 15 2017" />
 
         <Header title="Dynamic List" />
         {this.state.todos.map(todo => {
