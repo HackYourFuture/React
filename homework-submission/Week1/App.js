9187e4a1be41d045f2764c7ea1d90683b4859524
Akif <akbu2709@gmail.com>
2019-06-28 15:25:28 +0200

    Corrections after review

--- homework-submission/Week1/App.js
+++ homework-submission/Week1/App.js
@@ -1,6 +1,6 @@
 const ListItem = ({ description, deadline, className }) => {
   return (
-    <li class={className}>
+    <li className={className}>
       {description}, {deadline}
     </li>
   );
@@ -11,9 +11,9 @@ const StaticList = () => {
   return (
     <div>
       <ul>
-        {<ListItem description="Get out of bed" deadline="Wed Sep 13 2017" />}
-        {<ListItem description="Brush teeth" deadline="Thu Sep 14 2017" />}
-        {<ListItem description="Eat breakfast" deadline="Fri Sep 15 2017" />}
+        <ListItem description="Get out of bed" deadline="Wed Sep 13 2017" />
+        <ListItem description="Brush teeth" deadline="Thu Sep 14 2017" />
+        <ListItem description="Eat breakfast" deadline="Fri Sep 15 2017" />
       </ul>
     </div>
   );
