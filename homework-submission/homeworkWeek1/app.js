68933b311ec145bdaee6f014f86f088f4788ab6f
kani103 <you@example.com>
2019-07-02 15:17:54 +0200

    new changes as recommended

--- homework-submission/homeworkWeek1/app.js
+++ homework-submission/homeworkWeek1/app.js
@@ -10,9 +10,9 @@ const StaticList = () => {
     return (
         <div>
             <ul>
-               <TodoList description="Get out of bed" deadline="Wed Sep 13 2017" />}
-               <TodoList description="Brush teeth" deadline="Thu Sep 14 2017" />}
-               <TodoList description="Eat breakfast" deadline="Fri Sep 15 2017"/>}
+               <TodoList description="Get out of bed" deadline="Wed Sep 13 2017" />
+               <TodoList description="Brush teeth" deadline="Thu Sep 14 2017" />
+               <TodoList description="Eat breakfast" deadline="Fri Sep 15 2017"/>
             </ul>
         </div>
     )
