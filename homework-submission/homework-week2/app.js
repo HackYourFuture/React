88cb59004458ae2f63bd2bd91c50b2cf6d489a0f
unknown <muhannadjudah@gmail.com>
2019-07-07 19:00:07 +0200

    add react homework week2

---
+++ homework-submission/homework-week2/app.js
@@ -0,0 +1,74 @@
+const Item = ({ id, description, deadline, done }) => {
+  // a component has li element and the data which comes from another components(static and dynamic)
+  if (done == true) {
+    return (
+      <li className="true">
+        {id} {description} {deadline}
+      </li>
+    );
+  } else {
+    return (
+      <li>
+        {id} {description} {deadline}
+      </li>
+    );
+  }
+};
+
+// a component has static data that will be passed to Item component
+const StaticList = () => {
+  return (
+    <ul className="static-list">
+      <Item description={'Get out of bed'} deadline={'Wed Sep 13 2017'} />
+      <Item description={'Brush teeth'} deadline={'Thu Sep 14 2017'} />
+      <Item description={'Eat breakfast'} deadline={'Fri Sep 15 2017'} />
+      <Item description={'Play sport'} deadline={'Sat Sep 16 2017'} />
+      <Item description={'Do homework'} deadline={'Sun Sep 17 2017'} />
+      <Item description={'Go to party'} deadline={'Mon Sep 18 2017'} />
+    </ul>
+  );
+};
+
+// a component has static data that comes from App class then will be passed to Item component
+const DynamicList = ({ data }) => {
+  const result = data.map(({ id, done, description, deadline }) => {
+    return <Item key={id} id={id} description={description} deadline={deadline} done={done} />;
+  });
+  return <ul className="dynamic-list">{result}</ul>;
+};
+
+// a class component inherits all methods from react component, it works as container for all above components
+class App extends React.Component {
+  todoObject = [
+    {
+      id: 1,
+      description: 'Get out of bed',
+      deadline: '2017-09-11',
+      done: true,
+    },
+    {
+      id: 2,
+      description: 'Brush teeth',
+      deadline: '2017-09-10',
+      done: false,
+    },
+    {
+      id: 3,
+      description: 'Eat breakfast',
+      deadline: '2017-09-09',
+      done: false,
+    },
+  ];
+
+  render() {
+    return (
+      <div>
+        <h2>Static List</h2>
+        <StaticList />
+        <h2>Dynamic List</h2>
+        <DynamicList data={this.todoObject} />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById('thisIsWhereReactIsInjected'));
