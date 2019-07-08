4bc4b89aec79ae0d43688e4181cb84ce3591c8b5
soolaf <harkysoolaf@gmail.com>
2019-07-08 18:47:41 +0200

    update homework

--- homework-submission/main.js
+++ homework-submission/main.js
@@ -41,37 +41,36 @@ class TodoList extends React.Component {
   addItem = element => {
     element.preventDefault();
     const newTodo = {
-      id: this.state.dynamicList.length + 1,
+      id: Math.random()
+        .toString(36)
+        .substr(2, 10),
       description: element.target.description.value,
       deadline: element.target.deadline.value,
       done: false,
     };
-    console.log(newTodo);
+
     this.setState({ dynamicList: this.state.dynamicList.concat(newTodo) });
   };
   removeItem = index => {
-    const newTodoList = this.state.dynamicList.splice(index, 1);
-    console.log(index);
+    const newTodoList = this.state.dynamicList.filter(item => item.id !== index);
 
-    this.setState({ todoList: newTodoList });
+    this.setState({ dynamicList: newTodoList });
   };
   render() {
+    console.log(this.state);
     return (
       <div>
         <h1>Dynamic List</h1>
         <AddingForm addTodoItem={this.addItem} />
 
-        {this.state.dynamicList.map((todoList, index) => (
-          <div className='todoList'>
-            <li
-              key={todoList.id}
-              style={
-                todoList.done === true ? { textDecoration: 'line-through' } : { textDecoration: '' }
-              }
-            >
-              {todoList.description},{todoList.deadline}
-              <button onClick={() => this.removeItem(index)}>remove</button>
-            </li>
+        {this.state.dynamicList.map(item => (
+          <div
+            className='todoList'
+            key={item.id}
+            style={item.done === true ? { textDecoration: 'line-through' } : { textDecoration: '' }}
+          >
+            {item.description},{item.deadline}
+            <button onClick={() => this.removeItem(item.id)}>remove</button>
           </div>
         ))}
       </div>
