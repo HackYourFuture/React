6cd9ccd4644dcd85fac1e42eabb6f0ad179cc750
unknown <muhannadjudah@gmail.com>
2019-07-13 11:27:37 +0200

    clean server file

--- homework-submission/react-hyf/server/server.js
+++ homework-submission/react-hyf/server/server.js
@@ -1,13 +1,6 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const axios = require('axios');
 
 const app = express();
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// const baseURL = `https://uinames.com/api/?amount=20`;
 
 app.use(express.static('../react-hyf-client/build'));
 
@@ -15,22 +8,8 @@ app.get('/', async (req, res) => {
   await res.sendfile('index.html', error => {
     throw error;
   });
-  // axios(baseURL)
-  //   .then(data => {
-  //     res.status(200).send(data.data);
-  //     res.json(data.data);
-  //   })
-  //   .catch(e => {
-  //     res.status(500).send({ error: 'sorry something went wrong' });
-  //   });
   res.end();
 });
-
-// test route to react page
-// app.get('/all_users', (req, res) => {
-//   res.status(200).send(data.data);
-//   res.json(data.data);
-// });
 
 const PORT = 4000;
 
