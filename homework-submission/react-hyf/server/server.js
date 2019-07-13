a4b69928c8cd6cfc1a9719d8fffa944a14b86451
unknown <muhannadjudah@gmail.com>
2019-07-13 11:23:25 +0200

    add settings  to server

--- homework-submission/react-hyf/server/server.js
+++ homework-submission/react-hyf/server/server.js
@@ -1,32 +1,38 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+// const bodyParser = require('body-parser');
+// const axios = require('axios');
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-const baseURL = `https://uinames.com/api/?amount=20`;
+// const baseURL = `https://uinames.com/api/?amount=20`;
 
-app.get('/users', (req, res) => {
-  axios(baseURL)
-    .then(data => {
-      res.status(200).send(data.data);
-      res.json(data.data);
-    })
-    .catch(e => {
-      res.status(500).send({ error: 'sorry something went wrong' });
-    });
+app.use(express.static('../react-hyf-client/build'));
+
+app.get('/', async (req, res) => {
+  await res.sendfile('index.html', error => {
+    throw error;
+  });
+  // axios(baseURL)
+  //   .then(data => {
+  //     res.status(200).send(data.data);
+  //     res.json(data.data);
+  //   })
+  //   .catch(e => {
+  //     res.status(500).send({ error: 'sorry something went wrong' });
+  //   });
+  res.end();
 });
 
 // test route to react page
-app.get('/all_users', (req, res) => {
-  res.status(200).send(data.data);
-  res.json(data.data);
-});
+// app.get('/all_users', (req, res) => {
+//   res.status(200).send(data.data);
+//   res.json(data.data);
+// });
 
-const PORT = 5000;
+const PORT = 4000;
 
 app.listen(PORT, () => {
   console.log(`running on ${PORT}`);
