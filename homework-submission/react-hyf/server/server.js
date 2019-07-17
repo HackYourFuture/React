43c39416e9758dfe077fa42ea2d563890d37e82b
eneserdek <enesf.erdek@gmail.com>
2019-07-17 13:42:58 +0200

    Express server is working. I don't know how to use concurrently.

--- homework-submission/react-hyf/server/server.js
+++ homework-submission/react-hyf/server/server.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('../react-hyf-client-build'));
+app.use(express.static('../react-hyf-client/build'));
+
+app.get('/', (req, res) => {
+  res.sendFile('../react-hyf-client/build/index', e => {
+    console.log(e);
+  });
+});
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
