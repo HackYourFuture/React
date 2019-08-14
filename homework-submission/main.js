51e3813d6f25a3666d67bab24da7d352724a59ac
shybc <suheyb.c@gmail.com>
2019-08-15 00:12:27 +0200

    got rid of unnecessary  and made small changes to organize and shorten the code

--- homework-submission/main.js
+++ homework-submission/main.js
@@ -1,56 +1,5 @@
 'use strict';
 
-// The goal of this component is to take a parameter for list type as props
-// and insert a header with class of 'list-title' for lists
-const ListTitle = props => <h1 className="list-title">{props.type} List</h1>;
-
-// The goal of this component is to insert a list item with given description and
-// deadline, to give this inserted item proper class names, to add an event listener
-// that when a todo is clicked crosses it out as done if the todo is not done
-// and vice versa
-class ListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { done: props.done ? props.done : false };
-    this.changeDoneState = this.changeDoneState.bind(this);
-  }
-
-  changeDoneState() {
-    if (this.state.done === true) {
-      this.setState({ done: false });
-    } else {
-      this.setState({ done: true });
-    }
-  }
-
-  render() {
-    const { description, deadline } = this.props;
-    const date = new Date(deadline);
-    const dateOptions = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
-    return (
-      <li onClick={this.changeDoneState} className={`list-item ${this.state.done}`}>
-        {description}, {date.toLocaleDateString('en-EN', dateOptions)}
-      </li>
-    );
-  }
-}
-
-// The goal of this component is to insert a static-type list with static list items
-class StaticList extends React.Component {
-  render() {
-    return (
-      <div className="list-container">
-        <ListTitle type="Static" />
-        <ul className="list static">
-          <ListItem description="Get out of bed" deadline="Wed Sep 13 2017" />
-          <ListItem description="Brush teeth" deadline="Thu Sep 14 2017" />
-          <ListItem description="Eat breakfast" deadline="Fri Sep 15 2017" />
-        </ul>
-      </div>
-    );
-  }
-}
-
 const todoListArr = [
   {
     id: 1,
@@ -72,6 +21,57 @@ const todoListArr = [
   },
 ];
 
+// The goal of this component is to take a parameter for list type as props
+// and insert a header with class of 'list-title' for lists
+const ListTitle = props => <h1 className="list-title">{props.type} List</h1>;
+
+// The goal of this component is to insert a list item with given description and
+// deadline, to give this inserted item proper class names, to add an event listener
+// that when a todo is clicked crosses it out as done if the todo is not done
+// and vice versa
+class ListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { done: props.done ? props.done : false };
+    this.changeDoneState = this.changeDoneState.bind(this);
+  }
+
+  changeDoneState() {
+    this.setState({ done: this.state.done ? false : true });
+  }
+
+  render() {
+    const { description, deadline } = this.props;
+    const date = new Date(deadline).toLocaleDateString('en-EN', {
+      weekday: 'short',
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    });
+    return (
+      <li onClick={this.changeDoneState} className={`list-item ${this.state.done}`}>
+        {description}, {date}
+      </li>
+    );
+  }
+}
+
+// The goal of this component is to insert a static-type list with static list items
+class StaticList extends React.Component {
+  render() {
+    return (
+      <div className="list-container">
+        <ListTitle type="Static" />
+        <ul className="list static">
+          <ListItem description="Get out of bed" deadline="Wed Sep 13 2017" />
+          <ListItem description="Brush teeth" deadline="Thu Sep 14 2017" />
+          <ListItem description="Eat breakfast" deadline="Fri Sep 15 2017" />
+        </ul>
+      </div>
+    );
+  }
+}
+
 class DynamicList extends React.Component {
   render() {
     const { todoArr } = this.props;
@@ -79,9 +79,9 @@ class DynamicList extends React.Component {
       <div className="list-container">
         <ListTitle type="Dynamic" />
         <ul className="list dynamic">
-          {todoArr.map(todo => {
-            return <ListItem key={todo.id} {...todo} />;
-          })}
+          {todoArr.map(todo => (
+            <ListItem key={todo.id} {...todo} />
+          ))}
         </ul>
       </div>
     );
