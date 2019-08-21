ac43bdba17a15ab6a2d587a97d6042cc5574306c
veliataseven <veliataseven@gmail.com>
2019-08-21 02:05:26 +0200

    added Button component and validator function

--- homework-submission/homework-week1/homework-week2/App.js
+++ homework-submission/homework-week1/homework-week2/App.js
@@ -1,17 +1,20 @@
-const ListItem = ({ id, description, deadline, done, clickHandler }) => {
+const ListItem = ({ id, description, deadline, clickHandler, className }) => {
   return (
-    <li
-      key={id}
-      id={id}
-      className={done ? 'itemCompleted' : 'itemNotCompleted'}
-      onClick={clickHandler}
-    >
+    <li key={id} id={id} onClick={clickHandler} className={className}>
       {description} , {deadline}
     </li>
   );
 };
 
-const DynamicList = ({ List, clickHandler }) => {
+const Button = ({ type, text, className }) => {
+  return (
+    <button type={type} className={className}>
+      {text}
+    </button>
+  );
+};
+
+const DynamicList = ({ List, clickHandler, className }) => {
   return (
     <div className="list">
       {List.map(item => {
@@ -22,7 +25,7 @@ const DynamicList = ({ List, clickHandler }) => {
             id={item.id}
             description={item.description}
             deadline={item.deadline}
-            done={item.done}
+            className={className}
           />
         );
       })}
@@ -31,10 +34,15 @@ const DynamicList = ({ List, clickHandler }) => {
 };
 
 const AddingListItem = ({ submitHandler }) => {
+  let today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+  today = yyyy + '-' + mm + '-' + dd;
   return (
     <form onSubmit={submitHandler}>
-      <input type="text" name="listItem" />
-      <button type="submit">Add Item</button>
+      <input type="text" name="listItem" defaultValue={'Do something , ' + today} />
+      <Button type="submit" text="Add Item" className="addItem" />
     </form>
   );
 };
@@ -46,19 +54,16 @@ class App extends React.Component {
         id: 0,
         description: 'Get out of bed',
         deadline: '2017-09-11',
-        done: true,
       },
       {
         id: 1,
         description: 'Brush teeth',
         deadline: '2017-09-10',
-        done: false,
       },
       {
         id: 2,
         description: 'Eat breakfast',
         deadline: '2017-09-09',
-        done: false,
       },
     ],
   };
@@ -68,6 +73,9 @@ class App extends React.Component {
     const list = this.state.todoList;
     const copyList = [...list];
     const value = event.target.listItem.value;
+    const splitedValue = value.split(',');
+    let description = splitedValue[0];
+    let deadline = splitedValue[1];
     let idItem = 0;
     let today = new Date();
     const dd = String(today.getDate()).padStart(2, '0');
@@ -75,20 +83,37 @@ class App extends React.Component {
     const yyyy = today.getFullYear();
     today = yyyy + '-' + mm + '-' + dd;
 
+    const { descriptionValidation, deadlineValidation } = this.validator(description, deadline);
+    if (!deadlineValidation) deadline = today;
+    if (!descriptionValidation) description = 'Do something';
+
     if (list.length !== 0) {
       idItem = list[list.length - 1].id + 1;
     }
 
     copyList.push({
       id: idItem,
-      description: value,
-      deadline: today,
-      done: false,
+      description: description,
+      deadline: deadline,
     });
 
     this.setState({
       todoList: copyList,
     });
+  };
+
+  validator = (description, deadline) => {
+    const regex = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+    let deadlineValidation = true;
+    let descriptionValidation = true;
+
+    if (deadline == '' || deadline == null || deadline == undefined || !regex.test(deadline))
+      deadlineValidation = false;
+
+    if (description == '' || description == null || description == undefined)
+      descriptionValidation = false;
+
+    return { descriptionValidation: descriptionValidation, deadlineValidation: deadlineValidation };
   };
 
   removeItem = event => {
@@ -104,7 +129,11 @@ class App extends React.Component {
     return (
       <div className="wrapper">
         <AddingListItem submitHandler={this.addItem} />
-        <DynamicList List={this.state.todoList} clickHandler={this.removeItem} />
+        <DynamicList
+          List={this.state.todoList}
+          clickHandler={this.removeItem}
+          className="listItem"
+        />
       </div>
     );
   }
