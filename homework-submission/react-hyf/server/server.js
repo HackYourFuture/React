44f56b6ee23aec6f2dcb03b10c57645dc9b45936
Ghufran Deiri <ghufrandeiri@MacBook-Pro-van-Ghufran.local>
2019-09-07 19:34:32 +0200

    CRA and a backend server

--- homework-submission/react-hyf/server/server.js
+++ homework-submission/react-hyf/server/server.js
@@ -1,14 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const path = '../react-hyf-client/week3/build';
-app.use(express.static(path));
-
-app.get('/', (request, response) => {
-  response.sendFile(path / index, event => {
-    console.log(event);
-  });
-});
+const source = '../week3/build';
+app.use(express.static(path.join(__dirname, source)));
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
