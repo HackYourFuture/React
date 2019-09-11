d0b5aa465f7a8e8bd220597db273621a7de6db59
alquinex <alquinex2010@hotmail.com>
2019-09-11 07:57:54 +0200

    change

--- react-hyf/reactrouter/src/App.js
+++ react-hyf/reactrouter/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>React Router</h1>
+      </div>
+    </Router>
   );
 }
 
