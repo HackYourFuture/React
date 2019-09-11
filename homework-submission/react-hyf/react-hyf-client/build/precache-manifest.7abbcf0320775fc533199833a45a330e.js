56ac61cd3d03166fe78417fcc63d818760fd5fbc
Nadoshka <nadine.atike@gmail.com>
2019-09-11 19:40:52 +0200

    create react app

---
+++ homework-submission/react-hyf/react-hyf-client/build/precache-manifest.7abbcf0320775fc533199833a45a330e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f3ebed24409d9e953c0f5939f27369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a190cc9c1c06a8ce357f",
+    "url": "/static/css/main.c16d6035.chunk.css"
+  },
+  {
+    "revision": "60300ad7aa61e027af9e",
+    "url": "/static/js/2.4f77cdba.chunk.js"
+  },
+  {
+    "revision": "a190cc9c1c06a8ce357f",
+    "url": "/static/js/main.df29e0aa.chunk.js"
+  },
+  {
+    "revision": "562e5257e04e4a8f2307",
+    "url": "/static/js/runtime~main.4d0de26a.js"
+  }
+]);
