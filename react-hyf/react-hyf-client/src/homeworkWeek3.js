27f9a41287b176688fcbacf74088bead3d672b07
shybc <suheyb.c@gmail.com>
2019-09-21 00:00:18 +0200

    refined the code, got rid of hidden parts in state

--- react-hyf/react-hyf-client/src/homeworkWeek3.js
+++ react-hyf/react-hyf-client/src/homeworkWeek3.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function Header({ text = 'Press Spacebar', className }) {
-  return <h1 className={className}>{text}</h1>;
+function Header({ text = 'Press Spacebar', className = '' }) {
+  return <h1 className={`header ${className}`}>{text}</h1>;
 }
 
-function Image({ url, className, alt }) {
-  return <img src={url} className={`${className} avatar`} alt={alt} />;
+function Image({ url, alt }) {
+  return <img src={url} className="avatar" alt={alt} />;
 }
 
 function ListItem({ text }) {
   return <li className="list-item">{text}</li>;
 }
 
-function List({ person, className }) {
+function List({ person }) {
   const icons = ['👤', '🌐', '📞', '🎂', '✉️', '🔑'];
   const personArr = Object.keys(person).length
     ? [
@@ -27,8 +27,8 @@ function List({ person, className }) {
     : [];
 
   return (
-    <section className={`${className} list-container`}>
-      <ul className={`${className} list`}>
+    <section className="list-container">
+      <ul className="list">
         {personArr.map((info, index) => (
           <ListItem key={index} text={`${icons[index] || ' '} ${info}`} />
         ))}
@@ -42,17 +42,12 @@ class HomeworkWeek3 extends React.Component {
     super(props);
     this.state = {
       error: false,
-      hidden: 'hidden',
-      willHide: '',
       index: 0,
       people: [],
       person: {},
-      personName: '',
-      photo: '',
     };
 
-    this.spacePressed = this.spacePressed.bind(this);
-    this.selectPerson = this.selectPerson.bind(this);
+    this.handleKeydown = this.handleKeydown.bind(this);
   }
 
   async componentDidMount() {
@@ -62,21 +57,15 @@ class HomeworkWeek3 extends React.Component {
 
       this.setState({ people });
 
-      document.body.addEventListener('keydown', this.spacePressed);
+      document.body.addEventListener('keydown', this.handleKeydown);
     } catch (error) {
       this.setState({ error: true });
     }
   }
 
-  spacePressed(event) {
-    if (event.code === 'Space') {
-      this.setState({ hidden: '', willHide: 'hidden' });
+  async handleKeydown(event) {
+    event.preventDefault();
 
-      this.selectPerson();
-    }
-  }
-
-  async selectPerson() {
     const { people } = this.state;
     if (this.state.index > people.length - 1) {
       await this.setState({ index: 0 });
@@ -85,27 +74,26 @@ class HomeworkWeek3 extends React.Component {
 
     this.setState({
       person,
-      personName: `${person.name} ${person.surname}`,
-      photo: person.photo,
       index: this.state.index + 1,
     });
   }
 
   render() {
+    const { person, error } = this.state;
+
+    if (error) {
+      return <Header className={`error`} text="An error occurred. Try refresh." />;
+    }
+
     return (
       <div className="App">
-        {this.state.error ? (
-          <Header className={`error`} text="An error occurred. Try refresh." />
+        {Object.keys(person).length === 0 ? (
+          <Header />
         ) : (
           <main className="main">
-            <Header className={`${this.state.willHide}`} />
-            <Image
-              className={`${this.state.hidden}`}
-              url={this.state.photo}
-              alt={this.state.personName}
-            />
-            <Header className={`${this.state.hidden} name`} text={this.state.personName} />
-            <List className={`${this.state.hidden}`} person={this.state.person} />
+            <Image url={person.photo} alt={person.name} />
+            <Header className="name" text={`${person.name} ${person.surname}`} />
+            <List person={this.state.person} />
           </main>
         )}
       </div>
