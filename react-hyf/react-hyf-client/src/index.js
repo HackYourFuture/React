9a53ff7bff71502e84e9be822a63b71850b49d82
alquinex <alquinex2010@hotmail.com>
2019-09-22 22:26:38 +0200

    files deleted

--- react-hyf/react-hyf-client/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HomeworkWeek3 from './App';
-
-ReactDOM.render(<HomeworkWeek3 />, document.getElementById('root'));
--- react-hyf/reactrouter/src/index.js
+++ react-hyf/reactrouter/src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Files deleted
+
 ReactDOM.render(<App />, document.getElementById('root'));
