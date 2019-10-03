cdbd337c8553e97de875f5626f1ba918753cc047
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-10-03 17:37:37 +0200

    change ListBody className

--- homework-submission/week1/src/components/listBody/index.jsx
+++ homework-submission/week1/src/components/listBody/index.jsx
@@ -3,7 +3,7 @@ import ListItem from '../items';
 
 function ListBody({ items }) {
   return (
-    <ul className="listHeader">
+    <ul className="listBody">
       {items.map(item => {
         const { id, description, deadline, done } = item;
         return <ListItem id={id} description={description} deadline={deadline} done={done} />;
