ec8d6075ab599e1695fff4b54c3f2e06e98a6c30
niyas <hojaniyazhan@yahoo.com>
2019-10-13 12:18:45 +0200

    homework submission week2

--- homework-submission/hw-week1/src/index.js
+++ homework-submission/hw-week1/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const staticList = [
+  'Get out of bed, Wed Sep 13 2017',
+  'Brush teeth, Thu Sep 14 2017',
+  'Eat breakfast, Fri Sep 15 2017',
+];
+
 const dynamicList = [
   {
     id: 1,
@@ -25,55 +31,61 @@ const dynamicList = [
 // This component is header aimed for top of each list
 const Header = ({ name }) => <h1 className="header">{name}</h1>;
 
-// const Button = ({ text, clickHandler }) => {
-//   return <button onClick={clickHandler}>{text}</button>;
-// };
-
-const ItemField = ({ formHandler }) => {
+// This component renders static todo list
+const StaticComponent = () => {
   return (
-    <form onSubmit={formHandler}>
-      <input type="text" name="itemField" />
-      <button type="submit">Add Todo</button>
-    </form>
+    <div className="class-div">
+      <Header name="Static List" />
+      <ul>
+        <Todo description="Get out of bed" deadline="Wed Sep 13 2017" />
+        <Todo description="Brush teeth" deadline="Thu Sep 14 2017" />
+        <Todo description="Eat breakfast" deadline="Fri Sep 15 2017" />
+      </ul>
+    </div>
   );
 };
+
+// This component renders dynamic todo list
+const DynamicComponent = ({ items }) => {
+  return (
+    <div className="class-div">
+      <Header name="Dynamic List" />
+      <ul>
+        {items.map(elem => (
+          <Todo
+            key={elem.id}
+            description={elem.description}
+            deadline={elem.deadline}
+            done={elem.done}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+class Container extends React.Component {
+  render() {
+    return (
+      <div className="title">
+        <StaticComponent items={staticList} header="Static List" />
+        <DynamicComponent items={dynamicList} header="Dynamic List" />
+      </div>
+    );
+  }
+}
 
 // This component renders a todo item which contains description and deadline
 class Todo extends React.Component {
   constructor(props) {
     super(props);
     this.state = { done: this.props.done };
-    this.state = { todo: dynamicList };
     this.itemDone = this.itemDone.bind(this);
   }
 
   itemDone() {
     this.setState({ done: !this.state.done });
   }
-
-  handleSubmit = e => {
-    e.preventDefault();
-    if (!e.target.itemField.value) {
-      alert('Text field is empty, please enter todo');
-    } else {
-      const id = this.state.todo.length;
-      const object = {
-        id: id,
-        description: '',
-        deadline: new Date().toISOString().substr(0, 10),
-        done: false,
-      };
-      object.description = e.target.itemField.value;
-      this.state.todo.push(object);
-      this.setState(this.state.todo);
-      e.target.itemField.value = '';
-    }
-  };
-
-  removeItem = index => {
-    const newTodo = this.state.todo.splice(index, 1);
-    this.setState(newTodo);
-  };
 
   render() {
     const { description, deadline } = this.props;
@@ -87,40 +99,7 @@ class Todo extends React.Component {
           <span className={`block ${this.state.done && 'through'}`}>{description}</span>
           <span>{deadline}</span>
         </div>
-        <button onClick={() => this.removeItem(this.state.todo[0].done)}>remove</button>
       </li>
-    );
-  }
-}
-
-// This component renders dynamic todo list
-const DynamicComponent = ({ items }) => {
-  return (
-    <div className="class-div">
-      <Header name="Dynamic List" />
-      <div>
-        <ItemField formHandler={() => this.handleSubmit()} />
-        <ul>
-          {items.map(elem => (
-            <Todo
-              key={elem.id}
-              description={elem.description}
-              deadline={elem.deadline}
-              done={elem.done}
-            />
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-class Container extends React.Component {
-  render() {
-    return (
-      <div className="title">
-        <DynamicComponent items={dynamicList} header="Dynamic List" />
-      </div>
     );
   }
 }
