9fd1aef43b7423d42a01692a856fa0498c516084
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-10-16 14:36:50 +0200

    add res.json and jsObject as responce

--- homework-submission/week3_app/back_end/server.js
+++ homework-submission/week3_app/back_end/server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/hey', (req, res) => res.send('ho!'));
+app.use(express.json());
+
+app.get('/hey', (req, res) =>
+  res.json({
+    setps: [
+      { number: 1, description: 'Create React App.' },
+      { number: 2, description: 'Creat simple NodeJs Express Server.' },
+      { number: 3, description: 'Make connection between the both.' },
+      {
+        number: 4,
+        description:
+          'Test the connection: send HTTP request from React App, and then recive HTTP respond form Express server, as JSON object.',
+      },
+    ],
+  }),
+);
 
 app.listen(8080, () => console.log('The server is listening to: http://localhost:8080/'));
