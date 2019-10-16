1e0ce59d4023c2c4231a5854b3071e1ccdc864bb
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-10-16 19:23:48 +0200

    finish, but rendereing 3 times

--- homework-submission/week3_app/src/components/FetchForeign.js
+++ homework-submission/week3_app/src/components/FetchForeign.js
@@ -1,7 +1,46 @@
 import React, { useState, useEffect } from 'react';
 
 const DisplayUser = () => {
-  return <div>Display user</div>;
+  const [Loding, setLoding] = useState(true);
+  const [userInfo, setUserInfo] = useState({});
+
+  const clickHandel = () => setLoding(true);
+  console.log(Loding);
+
+  const getUser = async () => {
+    try {
+      const dataPromis = await fetch('https://uinames.com/api/?ext');
+      const dataUser = await dataPromis.json();
+      setUserInfo(dataUser);
+      setLoding(false);
+    } catch (error) {
+      console.log(error);
+    }
+    return function cleanup() {
+      console.log('clean');
+    };
+  };
+
+  useEffect(() => {
+    getUser();
+  }, [Loding]);
+  return Loding ? (
+    <p>'Loding'</p>
+  ) : (
+    <div>
+      <h3>Display user</h3>
+      <img src={userInfo.photo} alt="User" />
+      <br />
+      <button onClick={clickHandel}>click me</button>
+      <ul>
+        <li>Name: {userInfo.name}</li>
+        <li>Region: {userInfo.region}</li>
+        <li>Gender: {userInfo.gender}</li>
+        <li>Age: {userInfo.age}</li>
+        <li>Email: {userInfo.email}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default DisplayUser;
