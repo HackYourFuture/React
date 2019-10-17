d99b0bebd2ae13cb38ca2b36d366ae86a9433cc0
Salih <salihsert18@gmail.com>
2019-10-17 20:33:26 +0200

    destructred

--- homework-submission/react-hyf-client/src/components/UserData.js
+++ homework-submission/react-hyf-client/src/components/UserData.js
@@ -5,19 +5,19 @@ import { UserContext } from '../contexts/UserContext';
 
 const UserData = () => {
   const data = useContext(UserContext);
-
+  const { gender, region, phone, birthday, email, password } = data[1];
   return (
     data[0].loading === false && (
       <ul className="UserData card-body list-group list-group-flush ">
         <UserDataItem
-          icon={`fas fa-female ${data[1].gender === 'female' ? 'fa-female' : 'fa-male'}`}
-          info={data[1].gender}
+          icon={`fas fa-female ${gender === 'female' ? 'fa-female' : 'fa-male'}`}
+          info={gender}
         />
-        <UserDataItem icon={`fas fa-globe-asia`} info={data[1].region} />
-        <UserDataItem icon={`fas fa-phone`} info={data[1].phone} />
-        <UserDataItem icon={`fas fa-birthday-cake`} info={data[1].birthday.dmy} />
-        <UserDataItem icon={`fas fa-envelope`} info={data[1].email} />
-        <UserDataItem icon={`fas fa-key`} info={data[1].password} />
+        <UserDataItem icon={`fas fa-globe-asia`} info={region} />
+        <UserDataItem icon={`fas fa-phone`} info={phone} />
+        <UserDataItem icon={`fas fa-birthday-cake`} info={birthday.dmy} />
+        <UserDataItem icon={`fas fa-envelope`} info={email} />
+        <UserDataItem icon={`fas fa-key`} info={password} />
       </ul>
     )
   );
