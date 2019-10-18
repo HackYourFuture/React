a4c7c005f9909feb792081a0117e451125743634
elicarcar <elisabetta.carcar@gmail.com>
2019-10-18 05:09:31 +0200

    hosting the files on node

--- homework-submission/react-week3-v2/server.js
+++ homework-submission/react-week3-v2/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const port = 5000;
 
+app.use(express.static(path.join(__dirname, "build")));
 app.use(cors());
 
 app.listen(port, console.log("Hey you!"));
