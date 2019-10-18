8c8f56fa10ba963a9bdabc2e401f75f1714bee25
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-10-18 11:08:34 +0200

    fix rendering twice, and handel errors

--- homework-submission/week3_app/src/components/FetchForeign.js
+++ homework-submission/week3_app/src/components/FetchForeign.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const DisplayUser = () => {
   const [loding, setLoding] = useState(true);
-  const [userInfo, setUserInfo] = useState({});
+  const [userInfo, setUserInfo] = useState(null);
   const [error, setError] = useState(false);
 
   const clickHandel = () => {
@@ -26,11 +26,21 @@ const DisplayUser = () => {
     getUser();
   }, [loding]);
 
-  return loding ? (
-    <p> {error ? `Error: ${error.message}` : 'Loding'}</p>
+  return loding && !userInfo ? (
+    <div className={error ? 'on_screen warning' : 'none'}>
+      {error ? `Error: ${error.message}. server is busy, Try later` : 'Loding...................'}
+    </div>
   ) : (
     <div>
-      <h3> Display user (Fetch from remote server). </h3>
+      <div className={error ? 'on_screen warning' : 'none'}>
+        {error ? `Error: ${error.message}` : 'No Error'}
+        <br />
+        Can not get info for new user. Try later.
+      </div>
+      <h3 className={!error ? 'on_screen' : 'none'}>
+        {loding ? 'Loding new info' : 'Display user info'}
+      </h3>
+      <p>(Fetch from remote server)</p>
       <img src={userInfo.photo} alt="User" />
       <br />
 
