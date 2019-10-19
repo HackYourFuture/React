763a7a854435fb3812005461cb8289de45cd3f51
niyas <hojaniyazhan@yahoo.com>
2019-10-19 22:09:50 +0200

    homework submission week3

---
+++ react-hyf/react-hyf-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import ProfileGenerator from './ProfileGenerator';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ProfileGenerator />
+    </div>
+  );
+}
+
+export default App;
