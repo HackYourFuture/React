7fafa6ab32da0d8f45b991620aff5eca43f5694b
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-10-21 23:46:29 +0200

    add NavBar

--- homework-submission/react-week4/src/App.js
+++ homework-submission/react-week4/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import NavBar from './components/NavBar/NavBar';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header"></header>
     </div>
   );
