eb348105cb6e5223c1c01574dd6c3f7285f33623
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-10-22 23:07:13 +0200

    npm run build

---
+++ homework-submission/react-week4/build/precache-manifest.f62c22b0318536177fa0e46f2b146bc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e29bfbffbe834e9737d36962936272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7c8b0626e89a9ec0497",
+    "url": "/static/css/main.b122063c.chunk.css"
+  },
+  {
+    "revision": "e4aea99f0792701ebe44",
+    "url": "/static/js/2.e91b778e.chunk.js"
+  },
+  {
+    "revision": "d7c8b0626e89a9ec0497",
+    "url": "/static/js/main.951f222d.chunk.js"
+  },
+  {
+    "revision": "2d337efbc530765cdf3d",
+    "url": "/static/js/runtime-main.62cf6fba.js"
+  }
+]);
