f5761bd89fbbbc6a12c801ed3d6814230341b3e0
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-10-22 13:10:50 +0200

    add linksArray

---
+++ homework-submission/react-week4/src/components/NavBar/linksArray.js
@@ -0,0 +1,8 @@
+const linksArray = [
+  { href: '/', text: 'Home' },
+  { href: '/week1', text: 'Week1' },
+  { href: '/week2', text: 'Week2' },
+  { href: '/week3', text: 'Week3' },
+];
+
+export default linksArray;
