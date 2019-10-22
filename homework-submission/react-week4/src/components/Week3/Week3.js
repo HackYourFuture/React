3e0e064081c68080e53c74596c84f66a4c437334
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-10-22 21:23:37 +0200

    adjust style week3

--- homework-submission/react-week4/src/components/Week3/Week3.js
+++ homework-submission/react-week4/src/components/Week3/Week3.js
@@ -1,13 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Week3.css';
+// --------------
+
 const Week3 = () => {
-  return (
+  const [loding, setLoding] = useState(true);
+  const [userInfo, setUserInfo] = useState(null);
+  const [error, setError] = useState(false);
+
+  const clickHandel = () => {
+    setLoding(true);
+  };
+
+  const getUser = async () => {
+    try {
+      if (loding) {
+        const dataPromis = await fetch('https://uinames.com/api/?ext');
+        const dataUser = await dataPromis.json();
+        setUserInfo(dataUser);
+        setLoding(false);
+      }
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, [loding]);
+
+  return loding && !userInfo ? (
+    <div className={error ? 'w3_on_screen w3_warning' : 'w3_none '}>
+      {error ? `Error: ${error.message}. server is busy, Try later` : 'Loding...................'}
+    </div>
+  ) : (
     <div>
       <header className="App-header">
+        <div className={error ? 'w3_on_screen w3_warning' : 'w3_none '}>
+          {error ? `Error: ${error.message}` : 'No Error'}
+          <br />
+          Can not get info for new user. Try later.
+        </div>
         <h1>Week3</h1>
       </header>
-      <main className="App-main"></main>
+      <main className="App-main">
+        <h3 className={!error ? 'w3_on_screen' : 'w3_none '}>
+          {loding ? 'Loding new info' : 'Display user info'}
+        </h3>
+        <img src={userInfo.photo} alt="User" />
+        <br />
+
+        <ul>
+          <li>Name: {userInfo.name}</li>
+          <li>Region: {userInfo.region}</li>
+          <li>Gender: {userInfo.gender}</li>
+          <li>Age: {userInfo.age}</li>
+          <li>Email: {userInfo.email}</li>
+        </ul>
+        <button onClick={clickHandel}>Get another user</button>
+      </main>
     </div>
   );
 };
 
 export default Week3;
+
+// ------------------
+// const Week3 = () => {
+//   return (
+//     <div>
+//       <header className="App-header">
+//         <h1>Week3</h1>
+//       </header>
+//       <main className="App-main"></main>
+//     </div>
+//   );
+// };
+
+// export default Week3;
