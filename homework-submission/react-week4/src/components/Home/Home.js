dedaf734194bae4437fb55ef421c55e1b4fd4b74
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-10-22 22:58:41 +0200

    add Home css , adjust App css

--- homework-submission/react-week4/src/components/Home/Home.js
+++ homework-submission/react-week4/src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
@@ -6,7 +7,17 @@ const Home = () => {
       <header className="App-header">
         <h1>Home Page</h1>
       </header>
-      <main className="App-main"></main>
+      <main className="App-main">
+        <p className="home_p">
+          This small application is to summarize what I have learned during the Reat module.
+          <br />I upgraded my work, on the both sides : styling and React Script.
+          <br />
+          Everything looks better now. And there is also page 404, But I am wondering about : how to
+          give the browser something like a fake server's response with Status Code 404.
+          <br />I tried to change how I'm handling fetching new data after clicking on the button.
+          But I couldn't make it better. So it stays the same.
+        </p>
+      </main>
     </div>
   );
 };
