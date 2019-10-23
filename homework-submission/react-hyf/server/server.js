7bc3a5e25c67baefb991f3b1aecd21f19df025c5
aalhommada <aalhommada@gmail.com>
2019-10-23 10:00:56 +0200

    aalhommada-react-wweek4

--- homework-submission/react-hyf/server/server.js
+++ homework-submission/react-hyf/server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('../react-hyf-client/build'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
