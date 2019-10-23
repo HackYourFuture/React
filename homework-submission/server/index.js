1a80d3dc91a9eca3c277dfded8ced36348b844d8
Oguzhan <oguzhanturgut@outlook.com>
2019-10-23 17:05:13 +0200

    Remove server.js

--- homework-submission/server/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-
-app.use(express.static('../build'));
-
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
