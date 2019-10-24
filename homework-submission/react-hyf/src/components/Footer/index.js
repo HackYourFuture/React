f9e1176e2530eb57885d861b605ebae0536c1b30
Gul <gulpembeyener@gmail.com>
2019-10-24 15:31:44 +0200

    Fix footer

--- homework-submission/react-hyf/src/components/Footer/index.js
+++ homework-submission/react-hyf/src/components/Footer/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="page-footer font-small blue pt-4">
-      <div className="footer-copyright text-center py-3">
-        © 2019 Copyright: <strong>Gul Yener</strong>
-      </div>
+    <footer className="page-footer font-small blue py-3">
+      © 2019 Copyright: <strong>Gul Yener</strong>
     </footer>
   );
 }
