6bde9cd5d66ff3b99bc26ea3b61b7edbd73b0d61
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-10-24 08:27:06 +0200

    add last changes

--- homework-submission/react-hyf/react-hyf-client/src/folders/week3/HomeWorkWeek3.js
+++ homework-submission/react-hyf/react-hyf-client/src/folders/week3/HomeWorkWeek3.js
@@ -8,7 +8,7 @@ function HomeWorkWeek3() {
   const [index, setIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const randomNum = Math.floor(Math.random() * 10);
-  const userValues = Object.values(user);
+
   useEffect(() => {
     const getUsers = async () => {
       const res = await fetch(`https://uinames.com/api/?amount=10&&ext=photos`);
@@ -42,8 +42,8 @@ function HomeWorkWeek3() {
         <div className="loading-spinner"></div>
       ) : (
         <div className="container">
-          <ProfilPicture userValues={userValues} />
-          <UserInfo userValues={userValues} />
+          <ProfilPicture user={user} />
+          <UserInfo user={user} />
           <img
             className={'imgOnSmallScreen'}
             src={'https://img.icons8.com/metro/16/000000/change-user-male.png'}
