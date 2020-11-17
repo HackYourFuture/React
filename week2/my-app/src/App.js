749e58d0f986fd60b5865027ccf407c2270c7824
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2020-11-17 22:32:41 +0100

    Finished exercise 3: random joke generator

--- week2/my-app/src/App.js
+++ week2/my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Friend from './components/Friends';
 import DogGallery from './components/DogGallery';
+import RandomJoke from './components/RandomJoke';
 
 function App() {
   return (
     <div className="App">
      <Friend />
      <DogGallery />
+     <RandomJoke />
     </div>
   );
 }
