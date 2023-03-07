6aadaf81aeac2233a2a56065b76ed412298dba23
svetikzp <s.ostryanskaya@gmail.com>
2023-03-07 17:25:23 +0100

    Add files

--- week1/prep-exercises/1-counter/src/App.js
+++ week1/prep-exercises/1-counter/src/App.js
@@ -1,4 +1,3 @@
-//import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
 
--- week1/prep-exercises/1-counter/src/components/Count.jsx
+++ week1/prep-exercises/1-counter/src/components/Count.jsx
@@ -1,4 +1,5 @@
 
+
 export default function Count({number}) {
     return <div>{number}</div>
 
