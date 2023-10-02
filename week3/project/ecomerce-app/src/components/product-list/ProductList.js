bda636ee5d9c96fe1f9d943e640e854068f8ce78
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-10-02 22:33:47 +0200

    modified ProductList.js'

--- week3/project/ecomerce-app/src/components/product-list/ProductList.js
+++ week3/project/ecomerce-app/src/components/product-list/ProductList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './productList.css';
+import FavoriteButton from './favorite-button/FavoriteButton';
 
 function ProductList({ products }) {
   const limitedProducts = products.slice(0, 10);
@@ -16,6 +17,7 @@ function ProductList({ products }) {
           <div className="product">
             <img src={product.image} alt={product.title} />
             <h3>{product.title}</h3>
+            <FavoriteButton productId={product.id} />
           </div>
         </Link>
       ))}
