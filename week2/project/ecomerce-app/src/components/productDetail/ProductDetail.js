038560dde6d82d735213a898002127a9727aae94
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-10-02 21:56:23 +0200

    modified  ProductDetail.js

--- week2/project/ecomerce-app/src/components/productDetail/ProductDetail.js
+++ week2/project/ecomerce-app/src/components/productDetail/ProductDetail.js
@@ -1,51 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import './productList.css';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function ProductDetail() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetchProductDetails(id);
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch product details');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProduct(data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
   }, [id]);
 
-  const fetchProductDetails = async (productId) => {
-    setLoading(true);
-    try {
-      const response = await fetch(
-        `https://fakestoreapi.com/products/${productId}`
-      );
-      const data = await response.json();
-      setProduct(data);
-      setLoading(false);
-    } catch (err) {
-      setError(err);
-      setLoading(false);
-    }
-  };
-
   return (
-    <div className="product-detail">
-      {loading ? (
-        <p>Loading product details...</p>
-      ) : error ? (
-        <p>Error: {error.message}</p>
-      ) : product ? (
-        <div>
-          <h2>{product.title}</h2>
-          <img src={product.image} alt={product.title} />
-          <p>Price: ${product.price}</p>
-          <p>Category: {product.category}</p>
-          <div className="rating">
-            <p>Rating: {product.rating.rate}</p>
-            <p>Reviews: {product.rating.count}</p>
-          </div>
-          <p>Description: {product.description}</p>
-        </div>
+    <div>
+      {error ? (
+        <p>Error: {error}</p>
       ) : (
-        <p>No product details available.</p>
+        product && (
+          <div className="product__details">
+            <img src={product.image} alt={product.title} />
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+
+          </div>
+        )
       )}
     </div>
   );
