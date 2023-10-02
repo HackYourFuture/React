f94ce423b9d69c0ce4878da64a5a33b889fa151f
mehran banbersta <mehranbanbersta@mehrans-mbp.kpn>
2023-10-02 21:51:09 +0200

    modifyed app.js

--- week2/project/ecomerce-app/src/App.js
+++ week2/project/ecomerce-app/src/App.js
@@ -1,69 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import CategoryList from '../src/components/category-list/CategoryList';
-import ProductList from '../src/components/product-list/ProductList';
-import ProductDetail from '../src/components/productDetail/ProductDetail';
-// import LoadingIndicator from '../src/components/loading-indicator/LoadingIndicator';
-// import ErrorIndicator from '../src/components/error-indicator/ErrorIndicator';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CategoryList from './components/category-list/CategoryList';
+import ProductList from './components/product-list/ProductList';
+import ProductDetail from './components/productDetail/ProductDetail';
 
 function App() {
   const [activeCategory, setActiveCategory] = useState(null);
-  const [filteredProducts, setFilteredProducts] = useState([]);
   const [categories, setCategories] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // Fetch categories
-    fetchCategories();
-    fetchProducts();
+    fetch('https://fakestoreapi.com/products/categories')
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data);
+      })
+      .catch((error) => console.error('Error fetching categories:', error));
+
+    // Fetch products
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        setFilteredProducts(data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.error('Error fetching products:', error));
   }, []);
 
-  const fetchCategories = async () => {
-    try {
-      const response = await fetch(
-        'https://fakestoreapi.com/products/categories'
+  function filterProductsByCategory(category) {
+    setActiveCategory(category);
+    if (category === null) {
+      setFilteredProducts(products);
+    } else {
+      const filtered = products.filter(
+        (product) =>
+          product.category.replace('FAKE: ', '') ===
+          category.replace('FAKE: ', '')
       );
-      const data = await response.json();
-      setCategories(data);
-    } catch (err) {
-      setError(err);
+      setFilteredProducts(filtered);
     }
-  };
-
-  const fetchProducts = async (category = null) => {
-    setLoading(true);
-    try {
-      let endpoint = 'https://fakestoreapi.com/products';
-      if (category) {
-        endpoint = `https://fakestoreapi.com/products/category/${category}`;
-      }
-      const response = await fetch(endpoint);
-      const data = await response.json();
-      setFilteredProducts(data);
-      setLoading(false);
-    } catch (err) {
-      setError(err);
-      setLoading(false);
-    }
-  };
+  }
 
   return (
     <Router>
       <div className="App">
         <h1>Product Catalog</h1>
-        <CategoryList
-          categories={categories}
-          activeCategory={activeCategory}
-          onCategoryClick={(category) => {
-            setActiveCategory(category);
-            fetchProducts(category);
-          }}
-    />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/product/:id"
+            element={<ProductDetail products={products} />}
+          />
+        </Routes>
       </div>
     </Router>
   );
+
+  function Home() {
+    return (
+      <>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <CategoryList
+              categories={categories}
+              activeCategory={activeCategory}
+              onCategoryClick={filterProductsByCategory}
+            />
+            <ProductList products={filteredProducts} />
+          </>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
