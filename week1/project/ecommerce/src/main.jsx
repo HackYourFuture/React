27662db5db15ef03f64f8b59908d6bf7dcdff0d9
Moayad Mohammed <moayad.ashabi@gmail.com>
2024-09-01 12:03:35 +0200

    update main.jsx

--- week1/project/ecommerce/src/main.jsx
+++ week1/project/ecommerce/src/main.jsx
@@ -1,13 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render(
     <App />
-  </StrictMode>
-);
+)
